af211375ccd1481c79059add5230b7c88fa9a706
rklymenko <unnamzajazy@gmail.com>
2018-10-12 00:24:19 +0300

    -- Network layer refactored. In progress

--- postchain-base/src/main/kotlin/net/postchain/network/PeerConnectionAcceptor.kt
+++ postchain-base/src/main/kotlin/net/postchain/network/PeerConnectionAcceptor.kt
@@ -8,59 +8,13 @@ import net.postchain.network.ref.netty.NettyPeerConnection
 import java.net.ServerSocket
 import kotlin.concurrent.thread
 
-class PeerConnectionAcceptor(
-        peer: PeerInfo,
-        val IdentPacketConverter: IdentPacketConverter,
-        val registerConn: (IdentPacketInfo, PeerConnection) -> (ByteArray) -> Unit
-
-) {
-    val serverSocket: ServerSocket
-    @Volatile
-    var keepGoing = true
-
-    companion object : KLogging()
-
-    init {
-        if (peer is DynamicPortPeerInfo) {
-            serverSocket = ServerSocket(0)
-            peer.portAssigned(serverSocket.localPort)
-        } else {
-            serverSocket = ServerSocket(peer.port)
-        }
-        logger.info("Starting server on port ${peer.port} done")
-        thread(name = "-acceptLoop") { acceptLoop() }
-    }
-
-    private fun acceptLoop() {
-        try {
-            while (keepGoing) {
-                val socket = serverSocket.accept()
-                logger.info("accept socket")
-                PassivePeerConnection(
-                        IdentPacketConverter,
-                        socket,
-                        registerConn
-                )
-            }
-        } catch (e: Exception) {
-            logger.error("exiting accept loop")
-        }
-    }
-
-    fun stop() {
-        keepGoing = false
-        serverSocket.close()
-    }
-
-}
-
 //class PeerConnectionAcceptor(
-//        val peer: PeerInfo,
+//        peer: PeerInfo,
 //        val IdentPacketConverter: IdentPacketConverter,
-//        val registerConn: (IdentPacketInfo, NettyPeerConnection) -> (ByteArray) -> Unit
+//        val registerConn: (IdentPacketInfo, PeerConnection) -> (ByteArray) -> Unit
 //
 //) {
-//    // val serverSocket: ServerSocket
+//    val serverSocket: ServerSocket
 //    @Volatile
 //    var keepGoing = true
 //
@@ -68,24 +22,26 @@ class PeerConnectionAcceptor(
 //
 //    init {
 //        if (peer is DynamicPortPeerInfo) {
-//            //   serverSocket = ServerSocket(0)
-//            //   peer.portAssigned(serverSocket.localPort)
+//            serverSocket = ServerSocket(0)
+//            peer.portAssigned(serverSocket.localPort)
 //        } else {
-//            //        serverSocket = ServerSocket(peer.port)
+//            serverSocket = ServerSocket(peer.port)
 //        }
-//        //  logger.info("Starting server on port ${peer.port} done")
-//        //   acceptLoop()
-//        //   Thread({acceptLoop()}).start()
-//        acceptLoop()
+//        logger.info("Starting server on port ${peer.port} done")
+//        thread(name = "-acceptLoop") { acceptLoop() }
 //    }
 //
 //    private fun acceptLoop() {
 //        try {
-//            NettyPassivePeerConnection(
-//                    peer,
-//                    IdentPacketConverter,
-//                    registerConn
-//            )
+//            while (keepGoing) {
+//                val socket = serverSocket.accept()
+//                logger.info("accept socket")
+//                PassivePeerConnection(
+//                        IdentPacketConverter,
+//                        socket,
+//                        registerConn
+//                )
+//            }
 //        } catch (e: Exception) {
 //            logger.error("exiting accept loop")
 //        }
@@ -93,7 +49,52 @@ class PeerConnectionAcceptor(
 //
 //    fun stop() {
 //        keepGoing = false
-//        //    serverSocket.close()
+//        serverSocket.close()
 //    }
 //
 //}
+
+class PeerConnectionAcceptor(
+        val peer: PeerInfo,
+        val IdentPacketConverter: IdentPacketConverter,
+        val registerConn: (IdentPacketInfo, NettyPeerConnection) -> (ByteArray) -> Unit
+
+) {
+    // val serverSocket: ServerSocket
+    @Volatile
+    var keepGoing = true
+
+    companion object : KLogging()
+
+    var portId = 8080
+    init {
+        if (peer is DynamicPortPeerInfo) {
+            //   serverSocket = ServerSocket(0)
+               peer.portAssigned(portId++)
+        } else {
+            //        serverSocket = ServerSocket(peer.port)
+        }
+        //  logger.info("Starting server on port ${peer.port} done")
+        //   acceptLoop()
+        thread(name = "-acceptLoop"){acceptLoop()}
+   //     acceptLoop()
+    }
+
+    private fun acceptLoop() {
+        try {
+            NettyPassivePeerConnection(
+                    peer,
+                    IdentPacketConverter,
+                    registerConn
+            )
+        } catch (e: Exception) {
+            logger.error("exiting accept loop")
+        }
+    }
+
+    fun stop() {
+        keepGoing = false
+        //    serverSocket.close()
+    }
+
+}
