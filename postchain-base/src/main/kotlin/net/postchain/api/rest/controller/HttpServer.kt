f3bf284db8d05fe1bd2199ffde96111bfaa88384
rklymenko <unnamzajazy@gmail.com>
2019-10-24 19:42:03 +0300

    -- Initial commit

--- postchain-base/src/main/kotlin/net/postchain/api/rest/controller/HttpServer.kt
+++ postchain-base/src/main/kotlin/net/postchain/api/rest/controller/HttpServer.kt
@@ -3,11 +3,11 @@ package net.postchain.api.rest.controller
 import mu.KLogging
 import net.postchain.api.rest.json.JsonFactory
 import net.postchain.core.UserMistake
-import spark.Service
+//import spark.Service
 
 open class HttpServer {
 
-    val http = Service.ignite()!!
+    //val http = Service.ignite()!!
     private var initilized = false
     companion object : KLogging()
 
@@ -15,56 +15,56 @@ open class HttpServer {
     private val gson = JsonFactory.makeJson()
 
     fun buildErrorHandler() {
-        http.exception(NotFoundError::class.java) { error, _, response ->
-            logger.error("NotFoundError:", error)
-            response.status(404)
-            response.body(error(error))
-        }
-
-        http.exception(BadFormatError::class.java) { error, _, response ->
-            logger.error("BadFormatError:", error)
-            response.status(400)
-            response.body(error(error))
-        }
-
-        http.exception(UserMistake::class.java) { error, _, response ->
-            logger.error("UserMistake:", error)
-            response.status(400)
-            response.body(error(error))
-        }
-
-        http.exception(OverloadedException::class.java) { error, _, response ->
-            response.status(503) // Service unavailable
-            response.body(error(error))
-        }
-
-        http.exception(Exception::class.java) { error, _, response ->
-            logger.error("Exception:", error)
-            response.status(500)
-            response.body(error(error))
-        }
-
-        http.notFound { _, _ -> error(UserMistake("Not found")) }
+//        http.exception(NotFoundError::class.java) { error, _, response ->
+//            logger.error("NotFoundError:", error)
+//            response.status(404)
+//            response.body(error(error))
+//        }
+//
+//        http.exception(BadFormatError::class.java) { error, _, response ->
+//            logger.error("BadFormatError:", error)
+//            response.status(400)
+//            response.body(error(error))
+//        }
+//
+//        http.exception(UserMistake::class.java) { error, _, response ->
+//            logger.error("UserMistake:", error)
+//            response.status(400)
+//            response.body(error(error))
+//        }
+//
+//        http.exception(OverloadedException::class.java) { error, _, response ->
+//            response.status(503) // Service unavailable
+//            response.body(error(error))
+//        }
+//
+//        http.exception(Exception::class.java) { error, _, response ->
+//            logger.error("Exception:", error)
+//            response.status(500)
+//            response.body(error(error))
+//        }
+//
+//        http.notFound { _, _ -> error(UserMistake("Not found")) }
     }
 
     fun initialize(listenPort: Int, sslCertificate: String? = null, sslCertificatePassword: String? = null) {
         if (!initilized) {
             logger.info ("HttpServer is being initialized....")
-            http.port(listenPort)
-            if (sslCertificate != null) {
-                http.secure(sslCertificate, sslCertificatePassword, null, null)
-            }
-            http.before { req, res ->
-                res.header(HttpHelper.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-                res.header(HttpHelper.ACCESS_CONTROL_REQUEST_METHOD, "POST, GET, OPTIONS")
-                //res.header("Access-Control-Allow-Headers", "")
-                res.type("application/json")
-
-                // This is to provide compatibility with old postchain-client code
-                req.pathInfo()
-                        .takeIf { it.endsWith("/") }
-                        ?.also { res.redirect(it.dropLast(1)) }
-            }
+//            http.port(listenPort)
+//            if (sslCertificate != null) {
+//                http.secure(sslCertificate, sslCertificatePassword, null, null)
+//            }
+//            http.before { req, res ->
+//                res.header(HttpHelper.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+//                res.header(HttpHelper.ACCESS_CONTROL_REQUEST_METHOD, "POST, GET, OPTIONS")
+//                //res.header("Access-Control-Allow-Headers", "")
+//                res.type("application/json")
+//
+//                // This is to provide compatibility with old postchain-client code
+//                req.pathInfo()
+//                        .takeIf { it.endsWith("/") }
+//                        ?.also { res.redirect(it.dropLast(1)) }
+//            }
             initilized = true
         }
 
@@ -72,7 +72,7 @@ open class HttpServer {
 
     fun stop() {
         try {
-            http.stop()
+            //http.stop()
             // Ugly hack to workaround that there is no blocking stop.
             // Test cases won't work correctly without it
             Thread.sleep(100)
