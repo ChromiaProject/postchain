64575340beff58342ef30df37371206a1f47dc73
Alex Mizrahi <alex.mizrahi@gmail.com>
2020-12-29 12:52:12 +0200

    refined L2 internal APIs

--- postchain-base/src/main/kotlin/net/postchain/base/snapshot/leaves.kt
+++ postchain-base/src/main/kotlin/net/postchain/base/snapshot/leaves.kt
@@ -2,3 +2,15 @@
 
 package net.postchain.base.snapshot
 
+import net.postchain.core.BlockEContext
+
+abstract class LeafStore {
+    abstract fun writeEvent(blockEContext: BlockEContext, data: ByteArray)
+    abstract fun writeState(blockEContext: BlockEContext, state_n: Long, data: ByteArray)
+
+    // delete events at and below given height
+    abstract fun pruneEvents(blockEContext: BlockEContext, height: Long)
+
+    // delete all states such that state_n is between left and right and state_height <= height
+    abstract fun pruneStates(blockEContext: BlockEContext, left: Long, right: Long, height: Long)
+}
