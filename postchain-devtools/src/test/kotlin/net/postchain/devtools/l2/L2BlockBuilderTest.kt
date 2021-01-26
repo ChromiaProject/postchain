e3178f04cb26999faeaa80a8aac76bd57c88a87e
Ha ĐANG <dvietha@gmail.com>
2021-01-26 20:32:44 +0700

    add test case that emit both event & account state

--- postchain-devtools/src/test/kotlin/net/postchain/devtools/l2/L2BlockBuilderTest.kt
+++ postchain-devtools/src/test/kotlin/net/postchain/devtools/l2/L2BlockBuilderTest.kt
@@ -257,6 +257,109 @@ class L2BlockBuilderTest : IntegrationTestSetup() {
         assertEquals(root2.toHex(), l2StateRoot2.toHex())
     }
 
+    @Test
+    fun testL2EventAndUpdateSnapshot() {
+        configOverrides.setProperty("infrastructure", "base/test")
+        val nodes = createNodes(1, "/net/postchain/devtools/l2/blockchain_config.xml")
+        val node = nodes[0]
+        val bcRid = systemSetup.blockchainMap[1]!!.rid // Just assume we have chain 1
+
+        fun enqueueTx(data: ByteArray): Transaction? {
+            try {
+                val tx = node.getBlockchainInstance().getEngine().getConfiguration().getTransactionFactory().decodeTransaction(data)
+                node.getBlockchainInstance().getEngine().getTransactionQueue().enqueue(tx)
+                return tx
+            } catch (e: Exception) {
+                logger.error(e) { "Can't enqueue tx" }
+            }
+            return null
+        }
+
+        var currentBlockHeight = -1L
+
+        fun sealBlock() {
+            currentBlockHeight += 1
+            buildBlockAndCommit(node.getBlockchainInstance().getEngine())
+            Assert.assertEquals(currentBlockHeight, getBestHeight(node))
+        }
+
+        // enqueue txs that emit event
+        val leafs = mutableListOf<Hash>()
+        for (i in 1..4) {
+            val l = i.toLong()
+            enqueueTx(makeL2EventOp(bcRid, l))
+            val event = GtvEncoder.simpleEncodeGtv(GtvFactory.gtv(
+                GtvInteger(l),
+                GtvByteArray(SECP256K1Keccak.digest(BigInteger.valueOf(l).toByteArray())))
+            )
+            leafs.add(SECP256K1Keccak.digest(event))
+        }
+
+        // enqueue txs that emit accounts' state
+        val leafHashes = mutableMapOf<Long, Hash>()
+        for (i in 0..15) {
+            val l = i.toLong()
+            enqueueTx(makeL2StateOp(bcRid, l))
+            val state = GtvEncoder.simpleEncodeGtv(GtvFactory.gtv(
+                GtvInteger(l),
+                GtvByteArray(SECP256K1Keccak.digest(BigInteger.valueOf(l).toByteArray()))))
+            leafHashes[l] = SECP256K1Keccak.digest(state)
+        }
+
+        // build 1st block and commit
+        sealBlock()
+
+        // calculate event root hash
+        val l12 = SECP256K1Keccak.treeHasher(leafs[0], leafs[1])
+        val l34 = SECP256K1Keccak.treeHasher(leafs[2], leafs[3])
+        val eventRootHash = SECP256K1Keccak.treeHasher(l12, l34)
+
+        // calculate state root hash
+        val l01 = SECP256K1Keccak.treeHasher(leafHashes[0]!!, leafHashes[1]!!)
+        val l23 = SECP256K1Keccak.treeHasher(leafHashes[2]!!, leafHashes[3]!!)
+        val hash00 = SECP256K1Keccak.treeHasher(l01, l23)
+        val l45 = SECP256K1Keccak.treeHasher(leafHashes[4]!!, leafHashes[5]!!)
+        val l67 = SECP256K1Keccak.treeHasher(leafHashes[6]!!, leafHashes[7]!!)
+        val hash01 = SECP256K1Keccak.treeHasher(l45, l67)
+        val l89 = SECP256K1Keccak.treeHasher(leafHashes[8]!!, leafHashes[9]!!)
+        val l1011 = SECP256K1Keccak.treeHasher(leafHashes[10]!!, leafHashes[11]!!)
+        val hash10 = SECP256K1Keccak.treeHasher(l89, l1011)
+        val l1213 = SECP256K1Keccak.treeHasher(leafHashes[12]!!, leafHashes[13]!!)
+        val l1415 = SECP256K1Keccak.treeHasher(leafHashes[14]!!, leafHashes[15]!!)
+        val hash11 = SECP256K1Keccak.treeHasher(l1213, l1415)
+        val leftHash = SECP256K1Keccak.treeHasher(hash00, hash01)
+        val rightHash = SECP256K1Keccak.treeHasher(hash10, hash11)
+        val stateRootHash = SECP256K1Keccak.treeHasher(leftHash, rightHash)
+        val root = stateRootHash.plus(eventRootHash)
+
+        // query state root from block header's extra data
+        val blockHeaderData = getBlockHeaderData(node, currentBlockHeight)
+        val extraData = blockHeaderData.gtvExtra
+        val l2RootHash = extraData["l2RootHash"]?.asByteArray()
+
+        assertEquals(root.toHex(), l2RootHash!!.toHex())
+
+        val l = 16L
+        enqueueTx(makeL2StateOp(bcRid, l))
+        val state = GtvEncoder.simpleEncodeGtv(GtvFactory.gtv(
+            GtvInteger(l),
+            GtvByteArray(SECP256K1Keccak.digest(BigInteger.valueOf(l).toByteArray()))))
+        leafHashes[l] = SECP256K1Keccak.digest(state)
+
+        // build 2nd block and commit
+        sealBlock()
+
+        // query state root from block header's extra data
+        val blockHeaderData2 = getBlockHeaderData(node, currentBlockHeight)
+        val extraData2 = blockHeaderData2.gtvExtra
+        val l2RootHash2 = extraData2["l2RootHash"]?.asByteArray()
+        val l2StateRoot2 = l2RootHash2!!.take(32).toByteArray()
+
+        // calculate state root in new block
+        val root2 = SECP256K1Keccak.treeHasher(stateRootHash, leafHashes[l]!!)
+        assertEquals(root2.toHex(), l2StateRoot2.toHex())
+    }
+
     private fun getBlockHeaderData(node: PostchainTestNode, height: Long): BlockHeaderData {
         val blockQueries = node.getBlockchainInstance().getEngine().getBlockQueries()
         val blockRid = blockQueries.getBlockRid(height).get()
