915810d28d65f89cc46178c9ca2bd39f802c2f35
Ha ĐANG <dvietha@gmail.com>
2021-02-26 17:35:29 +0700

    sample code to filter eth event logs

--- postchain-base/src/main/kotlin/net/postchain/ebft/BaseBlockManager.kt
+++ postchain-base/src/main/kotlin/net/postchain/ebft/BaseBlockManager.kt
@@ -15,7 +15,7 @@ import nl.komponents.kovenant.Promise
 /**
  * Manages intents and acts as a wrapper for [blockDatabase] and [statusManager]
  */
-class BaseBlockManager(
+open class BaseBlockManager(
         private val processName: BlockchainProcessName,
         val blockDB: BlockDatabase,
         val statusManager: StatusManager,
