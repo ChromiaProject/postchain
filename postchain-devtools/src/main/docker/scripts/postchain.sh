50d7f404fee06795b941b4c4f9fd5124b377b233
Linnéa Rosenbaum <linnea@rosenbaum.se>
2021-06-18 14:05:14 +0200

    updated to 3.3.2

--- postchain-devtools/src/main/docker/scripts/postchain.sh
+++ postchain-devtools/src/main/docker/scripts/postchain.sh
@@ -4,12 +4,6 @@ set -eu
 
 scriptdir=`dirname ${BASH_SOURCE[0]}`
 
-#find /
-#
-#echo " "
-#echo "scriptdir:"
-#echo scriptdir
-#find ./ -type dirname -name "maven" /
 #echo "${RELL_JAVA:-java} -Dlog4j.configurationFile=./scripts/bin/log4j2.yml -cp \"/maven/*\" net.postchain.AppKt $@"
 
 #${RELL_JAVA:-java} -Dlog4j.configurationFile=./scripts/bin/log4j2.yml -cp "/maven/*" net.postchain.AppKt $@
