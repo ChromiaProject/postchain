d46fcbd8a46d1233aa1bd0af3da4ad6c56d88150
Ha ĐANG <dvietha@gmail.com>
2021-06-24 18:17:36 +0700

    move SECP256K1Keccak to base

---
+++ postchain-base/src/main/kotlin/net/postchain/base/Hashes.kt
@@ -0,0 +1,42 @@
+package net.postchain.base
+
+import net.postchain.common.data.KECCAK256
+import java.nio.charset.StandardCharsets
+
+class Hashes {
+    companion object {
+        /**
+         * Keccak-256 hash function.
+         *
+         * @param input binary encoded input data
+         * @param offset of start of data
+         * @param length of data
+         * @return hash value
+         */
+        fun sha3(input: ByteArray?, offset: Int, length: Int): ByteArray {
+            val m = MessageDigestFactory.create(KECCAK256)
+            m.update(input, offset, length)
+            return m.digest()
+        }
+
+        /**
+         * Keccak-256 hash function.
+         *
+         * @param input binary encoded input data
+         * @return hash value
+         */
+        fun sha3(input: ByteArray): ByteArray {
+            return sha3(input, 0, input.size)
+        }
+
+        /**
+         * Keccak-256 hash function that operates on a UTF-8 encoded String.
+         *
+         * @param utf8String UTF-8 encoded string
+         * @return hash value as hex encoded string
+         */
+        fun sha3String(utf8String: String): String {
+            return Numeric.toHexString(sha3(utf8String.toByteArray(StandardCharsets.UTF_8)))
+        }
+    }
+}
