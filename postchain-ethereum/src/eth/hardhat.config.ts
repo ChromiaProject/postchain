88503cb2632e14273b0872354615ba1afcda4bee
Ha ĐANG <dvietha@gmail.com>
2021-08-18 15:54:13 +0700

    add solidity coverage report & some hardhat tasks

--- postchain-ethereum/src/eth/hardhat.config.ts
+++ postchain-ethereum/src/eth/hardhat.config.ts
@@ -1,5 +1,3 @@
-import { task } from "hardhat/config";
-
 import { config as dotenvConfig } from "dotenv";
 import { resolve } from "path";
 dotenvConfig({ path: resolve(__dirname, "./.env") });
@@ -14,6 +12,10 @@ import "@nomiclabs/hardhat-waffle";
 
 import "hardhat-gas-reporter";
 import "@nomiclabs/hardhat-etherscan";
+import "solidity-coverage";
+
+import "./tasks/clean";
+import "./tasks/accounts";
 
 const chainIds = {
   ganache: 1337,
@@ -28,17 +30,6 @@ const chainIds = {
 const MNEMONIC = process.env.MNEMONIC || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-const ALCHEMY_KEY = process.env.ALCHEMY_KEY || "";
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.getAddress());
-  }
-});
 
 function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig {
   const url: string = "https://" + network + ".infura.io/v3/" + INFURA_API_KEY;
@@ -56,7 +47,6 @@ function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig 
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
@@ -71,15 +61,27 @@ const config: HardhatUserConfig = {
     kovan: createTestnetConfig("kovan"),
     rinkeby: createTestnetConfig("rinkeby"),
     ropsten: createTestnetConfig("ropsten"),
+    coverage: {
+      url: "http://127.0.0.1:9999", // Coverage launches its own ganache-cli client
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
   },
   solidity: {
-    compilers: [
-      {
-        version: "0.7.6",
-      },
-      {
-        version: "0.7.6",
-      },
+    compilers: [{
+      version: "0.7.6",
+      settings: {
+        // Disable the optimizer when debugging
+        // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+        optimizer: {
+          enabled: true,
+          runs: 800,
+        }
+      }}
     ],
   },
   etherscan: {
@@ -88,7 +90,7 @@ const config: HardhatUserConfig = {
   gasReporter: {
     currency: "USD",
     gasPrice: 100,
-    // enabled: process.env.REPORT_GAS ? true : false,
+    enabled: process.env.REPORT_GAS ? true : false,
   },
   typechain: {
     outDir: "typechain",
