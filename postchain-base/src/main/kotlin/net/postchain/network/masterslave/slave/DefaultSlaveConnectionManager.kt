e9e5344539d87ff6b6278f3845234f512b5095b3
Eugene Tykulov <eugene.tykulov@gmail.com>
2021-08-24 14:17:26 +0300

    POS-164: Tests have been fixed

--- postchain-base/src/main/kotlin/net/postchain/network/masterslave/slave/DefaultSlaveConnectionManager.kt
+++ postchain-base/src/main/kotlin/net/postchain/network/masterslave/slave/DefaultSlaveConnectionManager.kt
@@ -10,7 +10,6 @@ import net.postchain.network.masterslave.MsConnection
 import net.postchain.network.masterslave.MsMessageHandler
 import net.postchain.network.masterslave.protocol.MsCodec
 import net.postchain.network.masterslave.protocol.MsDataMessage
-import net.postchain.network.masterslave.protocol.MsFindNextBlockchainConfigMessage
 import net.postchain.network.masterslave.protocol.MsMessage
 import net.postchain.network.masterslave.slave.netty.NettySlaveConnector
 import net.postchain.network.x.LazyPacket
@@ -26,9 +25,6 @@ import kotlin.concurrent.schedule
  */
 interface SlaveConnectionManager : XConnectionManager {
     fun setMsMessageHandler(chainId: Long, handler: MsMessageHandler)
-
-    @Deprecated("POS-164")
-    fun requestBlockchainConfig(chainId: Long)
     fun sendMessageToMaster(chainId: Long, message: MsMessage)
 }
 
@@ -216,16 +212,6 @@ class DefaultSlaveConnectionManager(
         msMessageHandlers[chainId] = handler
     }
 
-    override fun requestBlockchainConfig(chainId: Long) {
-        val chain = chains[chainId] ?: throw ProgrammerMistake("Master chain not found: $chainId")
-        if (chain.connection != null) {
-            val message = MsFindNextBlockchainConfigMessage(chain.config.blockchainRid.data, 0, null)
-            chain.connection?.sendPacket { MsCodec.encode(message) }
-        } else {
-            logger.error("${logger(chain)}: Can't send packet to master node blockchainRid = ${chain.config.blockchainRid}")
-        }
-    }
-
     override fun sendMessageToMaster(chainId: Long, message: MsMessage) {
         val chain = chains[chainId] ?: throw ProgrammerMistake("Master chain not found: $chainId")
         if (chain.connection != null) {
@@ -251,7 +237,7 @@ class DefaultSlaveConnectionManager(
     }
 
     private fun loggerPrefix(blockchainRid: BlockchainRid): String =
-        BlockchainProcessName(nodeConfig.pubKey, blockchainRid).toString()
+            BlockchainProcessName(nodeConfig.pubKey, blockchainRid).toString()
 
     private fun logger(descriptor: SlaveConnectionDescriptor): String = loggerPrefix(descriptor.blockchainRid)
 
--- postchain-base/src/test/kotlin/net/postchain/config/node/ManagedNodeConfigurationProviderTest.kt
+++ postchain-base/src/test/kotlin/net/postchain/config/node/ManagedNodeConfigurationProviderTest.kt
@@ -4,14 +4,10 @@ package net.postchain.config.node
 
 import assertk.assertions.containsExactly
 import assertk.assertions.isEmpty
-import assertk.assertions.isEqualTo
-import assertk.assertions.isSameAs
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import net.postchain.base.PeerInfo
-import net.postchain.base.peerId
 import net.postchain.common.hexStringToByteArray
-import net.postchain.config.app.AppConfig
 import net.postchain.network.x.XPeerID
 import org.junit.Test
 import java.time.Instant
@@ -35,7 +31,7 @@ class ManagedNodeConfigurationProviderTest {
             expected: Array<PeerInfo>
     ) {
         // Mock
-        val mockStorage = MockStorage.mock(manual)
+        val mockStorage = MockStorage.mockAppContext(manual)
 
         val mockManagedPeerInfos: PeerInfoDataSource = mock {
             on { getPeerInfos() } doReturn managed
@@ -84,7 +80,7 @@ class ManagedNodeConfigurationProviderTest {
     @Test
     fun getPeerInfoCollection__provider_returns_managedPeerInfos_iff_managedDataSource_isNotNull() {
         // Mock
-        val mockStorage = MockStorage.mock(emptyArray())
+        val mockStorage = MockStorage.mockAppContext(emptyArray())
 
         val mockManagedPeerInfos: PeerInfoDataSource = mock {
             on { getPeerInfos() } doReturn arrayOf(peerInfo0)
