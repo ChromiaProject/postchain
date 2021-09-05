abd6d74b787f7c083220e8320ef3aa129ebe0cfc
Ha ĐANG <dvietha@gmail.com>
2021-09-05 22:29:35 +0700

    support rinkeby testnet only

--- postchain-ethereum/src/eth/src/dapp/connectors.ts
+++ postchain-ethereum/src/eth/src/dapp/connectors.ts
@@ -2,5 +2,5 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 
 export const POLLING_INTERVAL = 12000;
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5],
+  supportedChainIds: [4],
 });
