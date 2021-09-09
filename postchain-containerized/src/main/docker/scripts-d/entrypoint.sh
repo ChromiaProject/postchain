7fa22551d2c58f5b879983eda20f978ff8f8ac7b
Eugene Tykulov <eugene.tykulov@gmail.com>
2021-09-09 17:38:27 +0300

    fabric8-et: no gosu, no postgres

--- postchain-containerized/src/main/docker/scripts-d/entrypoint.sh
+++ postchain-containerized/src/main/docker/scripts-d/entrypoint.sh
@@ -5,10 +5,10 @@ set -eu
 . ./env.sh
 
 echo "Starting postgres"
-bash postgres-entrypoint.sh postgres &
+#bash postgres-entrypoint.sh postgres &
 
 # Wait for postgres to finnish before launching postchain
-until netstat -tulpan |grep 5432|grep LISTEN ; do sleep 0.1; done > /dev/null 2>&1
+#until netstat -tulpan |grep 5432|grep LISTEN ; do sleep 0.1; done > /dev/null 2>&1
 
 # Launching a node
 echo "Launching subnode"
