afdfc9b7fca21c94cb9d23984b5103a64b41c5cb
Joar Svartholm <joar.svartholm@chromaway.com>
2021-12-09 11:52:42 +0100

    Close port after settings its props

--- postchain-devtools/src/main/kotlin/net/postchain/devtools/IntegrationTestSetup.kt
+++ postchain-devtools/src/main/kotlin/net/postchain/devtools/IntegrationTestSetup.kt
@@ -306,9 +306,10 @@ open class IntegrationTestSetup : AbstractIntegration() {
         val pubKeys = Array(nodeCount) { pubKey(it) } + Array(replicasCount) { pubKey(-it - 1) }
 
         peerInfos = pubKeys.map { Pair(it, ServerSocket(0)) }.map {
-            it.second.close()
             it.second.reuseAddress = true
-            PeerInfo(it.second.inetAddress.hostName, it.second.localPort, it.first)
+            val peer = PeerInfo(it.second.inetAddress.hostName, it.second.localPort, it.first)
+            it.second.close()
+            peer
         }.toTypedArray()
 
         return peerInfos!!
