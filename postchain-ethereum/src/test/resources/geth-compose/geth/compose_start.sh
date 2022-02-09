e9476f50937bf5239dd507b940f4b8b3ddc58f53
Robert Wideberg <robert.wideberg@chromaway.com>
2022-02-09 11:31:52 +0100

    Fix 403 invalid host issue + yarn test

--- postchain-ethereum/src/test/resources/geth-compose/geth/compose_start.sh
+++ postchain-ethereum/src/test/resources/geth-compose/geth/compose_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 geth --nousb init /geth/test.json
 geth --nousb account import /geth/key.txt --password /geth/password.txt
-geth --nousb --http --http.addr=0.0.0.0 --allow-insecure-unlock --unlock 0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 --password /geth/password.txt --mine --rpc.allow-unprotected-txs
+geth --nousb --http --http.addr=0.0.0.0 --http.vhosts=* --allow-insecure-unlock --unlock 0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 --password /geth/password.txt --mine --rpc.allow-unprotected-txs
