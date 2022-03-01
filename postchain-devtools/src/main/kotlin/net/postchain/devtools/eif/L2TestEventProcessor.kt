9b0464f17ba1f14bbde748ebbdadf2a0f223c008
Robert Wideberg <robert.wideberg@chromaway.com>
2022-03-01 13:58:24 +0100

    Separate test code from production code

---
+++ postchain-devtools/src/main/kotlin/net/postchain/devtools/eif/L2TestEventProcessor.kt
@@ -0,0 +1,50 @@
+package net.postchain.devtools.eif
+
+import net.postchain.base.snapshot.SimpleDigestSystem
+import net.postchain.common.data.KECCAK256
+import net.postchain.common.toHex
+import net.postchain.el2.EventProcessor
+import net.postchain.ethereum.contracts.ChrL2
+import net.postchain.gtv.Gtv
+import net.postchain.gtv.GtvFactory.gtv
+import net.postchain.gtx.OpData
+import org.web3j.abi.EventEncoder
+import java.math.BigInteger
+
+class L2TestEventProcessor : EventProcessor {
+
+    private val ds = SimpleDigestSystem(KECCAK256)
+
+    private var lastBlock = 0
+
+    override fun shutdown() {
+        return
+    }
+
+    override fun getEventData(): Pair<Array<Gtv>, List<Array<Gtv>>> {
+        val out = mutableListOf<Array<Gtv>>()
+        val start = lastBlock + 1
+        for (i in start..start + 10) {
+            lastBlock++
+            out.add(generateData(i.toLong(), i))
+        }
+        return Pair(arrayOf(), out)
+    }
+
+    override fun isValidEventData(ops: Array<OpData>): Boolean {
+        return true
+    }
+
+    private fun generateData(height: Long, i: Int): Array<Gtv> {
+        val blockHash = ds.digest(BigInteger.valueOf(i.toLong()).toByteArray()).toHex()
+        val transactionHash = ds.digest(BigInteger.valueOf((100 - i).toLong()).toByteArray()).toHex()
+        val contractAddress = ds.digest(BigInteger.valueOf(999L).toByteArray()).toHex()
+        val from = ds.digest(BigInteger.valueOf(1L).toByteArray()).toHex()
+        val to = ds.digest(BigInteger.valueOf(2L).toByteArray()).toHex()
+        return arrayOf(
+            gtv(height), gtv(blockHash), gtv(transactionHash),
+            gtv(i.toLong()), gtv(EventEncoder.encode(ChrL2.DEPOSITED_EVENT)),
+            gtv(contractAddress), gtv(from), gtv(to), gtv(BigInteger.valueOf(i.toLong()))
+        )
+    }
+}
