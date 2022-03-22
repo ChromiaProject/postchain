0870b698b0f10ab327692def80a87289cea1d076
Joar Svartholm <joar.svartholm@chromaway.com>
2022-03-22 11:21:24 +0100

    Make sure validation of a new block is ok only if current blocks timestamp is strictly before

--- postchain-base/src/test/kotlin/net/postchain/base/data/BaseBlockHeaderValidatorTest.kt
+++ postchain-base/src/test/kotlin/net/postchain/base/data/BaseBlockHeaderValidatorTest.kt
@@ -34,7 +34,7 @@ class BaseBlockHeaderValidatorTest {
         val myBlockData = InitialBlockData(myBlockchainRid,myBlockId, myChainIid, myPrevBlockRid, myHeight.toLong(), myTimestamp, arrayOf())
         val header = BaseBlockHeader.make(cryptoSystem, myBlockData, myMerkleRootHash, myTimestamp, mapOf())
 
-        val valid = GenericBlockHeaderValidator.advancedValidateAgainstKnownBlocks(header, myBlockData, ::expectedMerkleHash, ::getBlockRid, myTimestamp, 0)
+        val valid = GenericBlockHeaderValidator.advancedValidateAgainstKnownBlocks(header, myBlockData, ::expectedMerkleHash, ::getBlockRid, myTimestamp - 1, 0)
         assert(valid.result).isEqualTo(ValidationResult.Result.OK)
     }
 
