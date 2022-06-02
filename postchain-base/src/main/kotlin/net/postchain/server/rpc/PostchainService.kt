9dbce52ed375ff8b67587892cf055853f569bc06
Joar Svartholm <joar.svartholm@chromaway.com>
2022-06-02 18:07:53 +0200

    Fix throwables in server and start blockchain AFTER closing the write connection

--- postchain-base/src/main/kotlin/net/postchain/server/rpc/PostchainService.kt
+++ postchain-base/src/main/kotlin/net/postchain/server/rpc/PostchainService.kt
@@ -3,6 +3,7 @@ package net.postchain.server.rpc
 import io.grpc.stub.StreamObserver
 import net.postchain.PostchainNode
 import net.postchain.base.data.DatabaseAccess
+import net.postchain.base.data.DependenciesValidator
 import net.postchain.base.gtv.GtvToBlockchainRidFactory
 import net.postchain.base.withWriteConnection
 import net.postchain.gtv.GtvDecoder
@@ -15,14 +16,12 @@ class PostchainService(private val postchainNode: PostchainNode) : PostchainServ
         request: StartBlockchainRequest?,
         responseObserver: StreamObserver<StartBlockchainReply>?
     ) {
-        val replyBuilder = StartBlockchainReply.newBuilder()
-        if (request != null) {
-            postchainNode.startBlockchain(request.chainId)
-            replyBuilder.message = "Blockchain with id ${request.chainId} started"
-        } else {
-            replyBuilder.message = "Could not start chain"
-        }
-        responseObserver?.onNext(replyBuilder.build())
+        postchainNode.startBlockchain(request!!.chainId)
+        responseObserver?.onNext(
+            StartBlockchainReply.newBuilder()
+                .setMessage("Blockchain with id ${request.chainId} started")
+                .build()
+        )
         responseObserver?.onCompleted()
     }
 
@@ -44,26 +43,26 @@ class PostchainService(private val postchainNode: PostchainNode) : PostchainServ
         responseObserver: StreamObserver<AddConfigurationReply>?
     ) {
         if (!request!!.hasGtv() && !request.hasXml()) {
-            return responseObserver?.onError(Throwable("Both xml and gtv fields are empty"))!!
+            return responseObserver?.onError(io.grpc.Status.INVALID_ARGUMENT.withDescription("Both xml and gtv fields are empty").asRuntimeException())!!
         }
         withWriteConnection(postchainNode.postchainContext.storage, request.chainId) { ctx ->
             val db = DatabaseAccess.of(ctx)
             val hasConfig = db.getConfigurationData(ctx, request.height) != null
             if (hasConfig && !request.override) {
-                responseObserver?.onError(Throwable("Configuration already exists for height ${request.height} on chain ${request.chainId}"))
+                responseObserver?.onError(io.grpc.Status.ALREADY_EXISTS.withDescription("Configuration already exists for height ${request.height} on chain ${request.chainId}").asRuntimeException())
                 return@withWriteConnection false
             }
 
             val gtvConfiguration = if (request.hasXml()) GtvMLParser.parseGtvML(request.xml) else GtvDecoder.decodeGtv(request.gtv.toByteArray()) // Verify that gtv-format works
             db.addConfigurationData(ctx, request.height, GtvEncoder.encodeGtv(gtvConfiguration))
-            responseObserver?.onNext(
-                AddConfigurationReply.newBuilder().run {
-                    message = "Configuration height ${request.height} on chain ${request.chainId} has been added"
-                    build()
-                }
-            )
             true
         }
+        responseObserver?.onNext(
+            AddConfigurationReply.newBuilder().run {
+                message = "Configuration height ${request.height} on chain ${request.chainId} has been added"
+                build()
+            }
+        )
         responseObserver?.onCompleted()
     }
 
@@ -72,12 +71,12 @@ class PostchainService(private val postchainNode: PostchainNode) : PostchainServ
         responseObserver: StreamObserver<InitializeBlockchainReply>?
     ) {
         if (!request!!.hasGtv() && !request.hasXml()) {
-            return responseObserver?.onError(Throwable("Both xml and gtv fields are empty"))!!
+            return responseObserver?.onError(io.grpc.Status.INVALID_ARGUMENT.withDescription("Both xml and gtv fields are empty").asRuntimeException())!!
         }
         withWriteConnection(postchainNode.postchainContext.storage, request.chainId) { ctx ->
             val db = DatabaseAccess.of(ctx)
             if (db.getBlockchainRid(ctx) != null && !request.override) {
-                responseObserver?.onError(Throwable("Blockchain already exists"))
+                responseObserver?.onError(io.grpc.Status.ALREADY_EXISTS.withDescription("Blockchain already exists").asRuntimeException())
                 return@withWriteConnection false
             }
 
@@ -85,16 +84,16 @@ class PostchainService(private val postchainNode: PostchainNode) : PostchainServ
             val gtvConfiguration = if (request.hasXml()) GtvMLParser.parseGtvML(request.xml) else GtvDecoder.decodeGtv(request.gtv.toByteArray()) // Verify that gtv-format works
             val brid = GtvToBlockchainRidFactory.calculateBlockchainRid(gtvConfiguration)
             db.initializeBlockchain(ctx, brid)
+            DependenciesValidator.validateBlockchainRids(ctx, listOf())
             // TODO: Blockchain dependencies [DependenciesValidator#validateBlockchainRids]
             db.addConfigurationData(ctx, 0, GtvEncoder.encodeGtv(gtvConfiguration))
-            postchainNode.startBlockchain(request.chainId)
-            responseObserver?.onNext(InitializeBlockchainReply.newBuilder().run {
-                message = "Blockchain has been initialized"
-                build()
-            })
-
             true
         }
+        postchainNode.startBlockchain(request.chainId)
+        responseObserver?.onNext(InitializeBlockchainReply.newBuilder().run {
+            message = "Blockchain has been initialized"
+            build()
+        })
         responseObserver?.onCompleted()
     }
 }
