54c4bb7867eb5d8ef3301e8f3e546352ee7e73da
Ha ĐANG <dvietha@gmail.com>
2022-07-01 23:38:24 +0700

    fix build error

--- postchain-base/src/main/kotlin/net/postchain/config/node/ManualNodeConfigurationProvider.kt
+++ postchain-base/src/main/kotlin/net/postchain/config/node/ManualNodeConfigurationProvider.kt
@@ -2,11 +2,8 @@
 
 package net.postchain.config.node
 
-import net.postchain.base.PeerInfo
-import net.postchain.base.Storage
+import net.postchain.base.*
 import net.postchain.base.data.DatabaseAccess
-import net.postchain.base.peerId
-import net.postchain.base.withReadConnection
 import net.postchain.common.BlockchainRid
 import net.postchain.config.app.AppConfig
 import net.postchain.core.NodeRid
