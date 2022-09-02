a6e82e507b8fb0c6734c744c6231e4917d6cdcc0
Joar Svartholm <joar.svartholm@chromaway.com>
2022-09-02 16:00:55 +0200

    Clean up getting properties from env or file in client config

--- postchain-client/src/main/kotlin/net/postchain/client/config/PostchainClientConfig.kt
+++ postchain-client/src/main/kotlin/net/postchain/client/config/PostchainClientConfig.kt
@@ -4,6 +4,9 @@ import net.postchain.client.request.EndpointPool
 import net.postchain.common.BlockchainRid
 import net.postchain.common.PropertiesFileLoader
 import net.postchain.common.config.cryptoSystem
+import net.postchain.common.config.getEnvOrIntProperty
+import net.postchain.common.config.getEnvOrLongProperty
+import net.postchain.common.config.getEnvOrStringProperty
 import net.postchain.crypto.CryptoSystem
 import net.postchain.crypto.KeyPair
 import net.postchain.crypto.Secp256K1CryptoSystem
@@ -23,22 +26,16 @@ data class PostchainClientConfig(
         @JvmStatic
         fun fromProperties(propertiesFileName: String): PostchainClientConfig {
             val config = PropertiesFileLoader.load(propertiesFileName)
-            val pubkeys = (System.getenv("POSTCHAIN_CLIENT_PUBKEY")
-                ?: config.getString("pubkey", "")).split(",")
-            val privkeys = (System.getenv("POSTCHAIN_CLIENT_PRIVKEY")
-                ?: config.getString("privkey", "")).split(",")
-            require(pubkeys.size == privkeys.size) { "Equally manny pubkeys as privkeys must be provider, but ${pubkeys.size} and ${privkeys.size} was found"}
+            val pubkeys = config.getEnvOrStringProperty("POSTCHAIN_CLIENT_PUBKEY", "pubkey", "").split(",")
+            val privkeys = config.getEnvOrStringProperty("POSTCHAIN_CLIENT_PRIVKEY", "privkey", "").split(",")
+            require(pubkeys.size == privkeys.size) { "Equally many pubkeys as privkeys must be provided, but ${pubkeys.size} and ${privkeys.size} was found"}
             val signers = pubkeys.zip(privkeys).map { KeyPair.of(it.first, it.second) }
             return PostchainClientConfig(
-                blockchainRid = (System.getenv("POSTCHAIN_CLIENT_BLOCKCHAIN_RID")
-                    ?: config.getString("brid", "")).let { BlockchainRid.buildFromHex(it) },
-                endpointPool = EndpointPool((System.getenv("POSTCHAIN_CLIENT_API_URL")
-                    ?: config.getString("api.url", "")).split(",")),
+                blockchainRid = config.getEnvOrStringProperty("POSTCHAIN_CLIENT_BLOCKCHAIN_RID", "brid", "").let { BlockchainRid.buildFromHex(it) },
+                endpointPool = EndpointPool(config.getEnvOrStringProperty("POSTCHAIN_CLIENT_API_URL", "api.url", "").split(",")),
                 signers = signers,
-                statusPollCount = System.getenv("POSTCHAIN_CLIENT_STATUS_POLL_COUNT")?.toInt()
-                    ?: config.getInt("status.poll-count", STATUS_POLL_COUNT),
-                statusPollInterval = System.getenv("POSTCHAIN_CLIENT_STATUS_POLL_INTERVAL")?.toLong()
-                    ?: config.getLong("status.poll-interval", STATUS_POLL_INTERVAL),
+                statusPollCount = config.getEnvOrIntProperty("POSTCHAIN_CLIENT_STATUS_POLL_COUNT", "status.poll-count", STATUS_POLL_COUNT),
+                statusPollInterval = config.getEnvOrLongProperty("POSTCHAIN_CLIENT_STATUS_POLL_INTERVAL", "status.poll-interval", STATUS_POLL_INTERVAL),
                 cryptoSystem = config.cryptoSystem()
             )
         }
--- postchain-common/src/main/kotlin/net/postchain/common/config/extensions.kt
+++ postchain-common/src/main/kotlin/net/postchain/common/config/extensions.kt
@@ -8,5 +8,14 @@ import org.apache.commons.configuration2.Configuration
 fun Configuration.cryptoSystem() = getEnvOrStringProperty("POSTCHAIN_CRYPTO_SYSTEM", "crypto")?.let { newInstanceOf<CryptoSystem>(it) }
     ?: Secp256K1CryptoSystem()
 
-private fun Configuration.getEnvOrStringProperty(env: String, property: String) =
+fun Configuration.getEnvOrStringProperty(env: String, property: String): String? =
     System.getProperty(env) ?: getString(property)
+
+fun Configuration.getEnvOrStringProperty(env: String, property: String, default: String): String =
+    System.getProperty(env) ?: getString(property, default)
+
+fun Configuration.getEnvOrIntProperty(env: String, property: String, default: Int): Int =
+    System.getProperty(env)?.toInt() ?: getInt(property, default)
+
+fun Configuration.getEnvOrLongProperty(env: String, property: String, default: Long): Long =
+    System.getProperty(env)?.toLong() ?: getLong(property, default)
