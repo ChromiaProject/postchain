cbe8a6dc7e97a9416c37f53d2d4e4aa2931e7595
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-09-05 11:34:00 +0200

    Clean-up

--- postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfSpecialTxExtension.kt
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2022 ChromaWay AB. See README for license information.
-
-package net.postchain.d1.icmf
-
-interface IcmfSpecialTxExtension {
-    fun connectIcmfController(controller: IcmfController)
-}
