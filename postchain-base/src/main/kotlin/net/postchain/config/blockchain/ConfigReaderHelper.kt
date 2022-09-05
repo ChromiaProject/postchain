817e612bb615bd27520a49e6fadebc812713b713
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-09-05 12:06:06 +0200

    Simple config reader

---
+++ postchain-base/src/main/kotlin/net/postchain/config/blockchain/ConfigReaderHelper.kt
@@ -0,0 +1,33 @@
+package net.postchain.config.blockchain
+
+import net.postchain.common.BlockchainRid
+import net.postchain.common.exception.UserMistake
+import net.postchain.gtv.Gtv
+import net.postchain.gtv.GtvType
+
+object ConfigReaderHelper {
+
+
+    fun readAsBlockchainRid(setting: Gtv?, chainIid: Long, confKey: String): List<BlockchainRid> {
+        var ret = arrayListOf<BlockchainRid>()
+        if (setting != null) {
+            if (setting.type == GtvType.ARRAY) {
+                var i = 0
+                for (gtv in setting.asArray()) {
+                    if (gtv.type == GtvType.BYTEARRAY) {
+                        val bcRidGtv = gtv.asByteArray()
+                        if (bcRidGtv != null) {
+                            ret.add(BlockchainRid(bcRidGtv))
+                        }
+                    } else {
+                        throw UserMistake("Configuration error: $confKey 's array member pos $i should be Blockchain RIDs (hex), but was ${gtv.type}, chain: $chainIid.")
+                    }
+                    i++
+                }
+            } else {
+                throw UserMistake("Configuration error: $confKey was expected to hold an array, chain: $chainIid.")
+            }
+        }
+        return ret
+    }
+}
