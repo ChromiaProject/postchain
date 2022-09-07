218bee025c34e4f7507e774af36b9c69c16be777
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-09-07 17:02:52 +0200

    Small refactor

--- postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfMessage.kt
+++ postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfMessage.kt
@@ -23,25 +23,3 @@ data class IcmfMessage(
         return gtv("topic" to gtv(topic), "body" to body)
     }
 }
-
-/**
- * Conceptually = all messages related to a block
- */
-data class IcmfPacket(
-        val currentPointer: Gtv,
-        val height: Long, // Block height this package corresponds to
-        val blockRid: Gtv, // The BlockRid that goes with the header (for the cases where we cannot calculate it from the header)
-        val blockHeader: Gtv, // Header of the block
-        val witness: Gtv // Must send the witness so the recipient can validate
-) {
-
-    companion object {
-        /**
-         * @return a [IcmfPackage] without messages (Anchoring can use this)
-         */
-        fun build(height: Long, blockRid: Gtv, header: Gtv, witness: Gtv): IcmfPacket {
-            return IcmfPacket(gtv(height), height, blockRid, header, witness)
-        }
-    }
-
-}
