0106de3786e24809fcc60adc1f48b6db7ea6d934
Robert Wideberg <robert.wideberg@chromaway.com>
2022-09-07 11:24:40 +0200

    Remove messages from packet

--- postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfMessage.kt
+++ postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfMessage.kt
@@ -32,8 +32,7 @@ data class IcmfPacket(
         val height: Long, // Block height this package corresponds to
         val blockRid: Gtv, // The BlockRid that goes with the header (for the cases where we cannot calculate it from the header)
         val blockHeader: Gtv, // Header of the block
-        val witness: Gtv, // Must send the witness so the recipient can validate
-        val messages: List<IcmfMessage> // (potentially) messages
+        val witness: Gtv // Must send the witness so the recipient can validate
 ) {
 
     companion object {
@@ -41,8 +40,7 @@ data class IcmfPacket(
          * @return a [IcmfPackage] without messages (Anchoring can use this)
          */
         fun build(height: Long, blockRid: Gtv, header: Gtv, witness: Gtv): IcmfPacket {
-            return IcmfPacket(gtv(height),
-                    height, blockRid, header, witness, ArrayList())
+            return IcmfPacket(gtv(height), height, blockRid, header, witness)
         }
     }
 
