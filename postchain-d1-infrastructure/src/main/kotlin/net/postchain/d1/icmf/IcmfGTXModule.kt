90f8d92d6766b989a3fc3b008f37ecc35a6a13c0
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-09-12 10:22:24 +0200

    Rename icmf_message entity to avoid collision

--- postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfGTXModule.kt
+++ postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfGTXModule.kt
@@ -7,7 +7,7 @@ import net.postchain.crypto.Secp256K1CryptoSystem
 import net.postchain.gtx.SimpleGTXModule
 import net.postchain.gtx.special.GTXSpecialTxExtension
 
-fun DatabaseAccess.tableMessage(ctx: EContext) = tableName(ctx, "icmf_message")
+fun DatabaseAccess.tableMessage(ctx: EContext) = tableName(ctx, "sent_icmf_message")
 
 class IcmfGTXModule : SimpleGTXModule<Unit>(
     Unit,
