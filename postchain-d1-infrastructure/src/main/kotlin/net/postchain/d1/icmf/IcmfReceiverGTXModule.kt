afeb43ee754a1d9f49b16a7df684885da638420d
Eugene Tykulov <eugene.tykulov@chromaway.com>
2022-09-16 10:48:50 +0300

    POS-344: IcmfReceiverGTXModule and IcmfRemoteSTE have been made Directory Components

--- postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfReceiverGTXModule.kt
+++ postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfReceiverGTXModule.kt
@@ -4,6 +4,8 @@ import net.postchain.base.data.DatabaseAccess
 import net.postchain.core.EContext
 import net.postchain.gtx.SimpleGTXModule
 import net.postchain.gtx.special.GTXSpecialTxExtension
+import net.postchain.managed.DirectoryComponent
+import net.postchain.managed.DirectoryDataSource
 import org.apache.commons.dbutils.QueryRunner
 
 const val PREFIX: String = "sys.x.icmf" // This name should not clash with Rell
@@ -11,28 +13,38 @@ fun DatabaseAccess.tableAnchorHeight(ctx: EContext) = tableName(ctx, "${PREFIX}.
 fun DatabaseAccess.tableMessageHeight(ctx: EContext) = tableName(ctx, "${PREFIX}.message_height")
 
 class IcmfReceiverGTXModule(private val topics: List<String>) : SimpleGTXModule<Unit>(
-    Unit,
-    mapOf(),
-    mapOf()
-) {
+        Unit,
+        mapOf(),
+        mapOf()
+), DirectoryComponent {
+
+    private lateinit var directoryDataSource: DirectoryDataSource
 
     override fun initializeDB(ctx: EContext) {
         DatabaseAccess.of(ctx).apply {
             val queryRunner = QueryRunner()
             queryRunner.update(
-                ctx.conn,
-                "CREATE TABLE IF NOT EXISTS ${
-                    tableAnchorHeight(ctx)
-                } (cluster TEXT PRIMARY KEY, height BIGINT NOT NULL)"
+                    ctx.conn,
+                    "CREATE TABLE IF NOT EXISTS ${
+                        tableAnchorHeight(ctx)
+                    } (cluster TEXT PRIMARY KEY, height BIGINT NOT NULL)"
             )
             queryRunner.update(
-                ctx.conn,
-                "CREATE TABLE IF NOT EXISTS ${
-                    tableMessageHeight(ctx)
-                } (sender BYTEA, topic TEXT, height BIGINT NOT NULL, PRIMARY KEY (sender, topic))"
+                    ctx.conn,
+                    "CREATE TABLE IF NOT EXISTS ${
+                        tableMessageHeight(ctx)
+                    } (sender BYTEA, topic TEXT, height BIGINT NOT NULL, PRIMARY KEY (sender, topic))"
             )
         }
     }
 
-    override fun getSpecialTxExtensions(): List<GTXSpecialTxExtension> = listOf(IcmfRemoteSpecialTxExtension(topics))
+    override fun getSpecialTxExtensions(): List<GTXSpecialTxExtension> {
+        return listOf(IcmfRemoteSpecialTxExtension(topics)).onEach { ext ->
+            if (ext is DirectoryComponent) ext.setDirectoryDataSource(directoryDataSource)
+        }
+    }
+
+    override fun setDirectoryDataSource(directoryDataSource: DirectoryDataSource) {
+        this.directoryDataSource = directoryDataSource
+    }
 }
