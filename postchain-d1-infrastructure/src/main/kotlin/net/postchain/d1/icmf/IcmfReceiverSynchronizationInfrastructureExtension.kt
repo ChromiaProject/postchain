852babbb1e08bc39086a40f721e595b0248253c8
Robert Wideberg <robert.wideberg@chromaway.com>
2022-09-20 14:03:25 +0200

    Avoid casting engine by using context constructor param

--- postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfReceiverSynchronizationInfrastructureExtension.kt
+++ postchain-d1-infrastructure/src/main/kotlin/net/postchain/d1/icmf/IcmfReceiverSynchronizationInfrastructureExtension.kt
@@ -1,24 +1,22 @@
 package net.postchain.d1.icmf
 
-import net.postchain.base.BaseBlockchainEngine
+import net.postchain.PostchainContext
 import net.postchain.core.BlockchainProcess
 import net.postchain.core.SynchronizationInfrastructureExtension
 import net.postchain.gtx.GTXBlockchainConfiguration
 
-class IcmfReceiverSynchronizationInfrastructureExtension : SynchronizationInfrastructureExtension {
+class IcmfReceiverSynchronizationInfrastructureExtension(private val postchainContext: PostchainContext) : SynchronizationInfrastructureExtension {
     private val receivers = mutableMapOf<Long, GlobalTopicIcmfReceiver>()
 
     override fun connectProcess(process: BlockchainProcess) {
         val engine = process.blockchainEngine
-        if (engine is BaseBlockchainEngine) {
-            val configuration = engine.getConfiguration()
-            if (configuration is GTXBlockchainConfiguration) {
-                val topics = configuration.configData.rawConfig["icmf"]!!["receiver"]!!["topics"]!!.asArray().map { it.asString() }
-                val receiver = GlobalTopicIcmfReceiver(topics, configuration.cryptoSystem, engine.storage, configuration.chainID)
-                receivers[configuration.chainID] = receiver
+        val configuration = engine.getConfiguration()
+        if (configuration is GTXBlockchainConfiguration) {
+            val topics = configuration.configData.rawConfig["icmf"]!!["receiver"]!!["topics"]!!.asArray().map { it.asString() }
+            val receiver = GlobalTopicIcmfReceiver(topics, configuration.cryptoSystem, postchainContext.storage, configuration.chainID)
+            receivers[configuration.chainID] = receiver
 
-                getIcmfRemoteSpecialTxExtension(configuration)?.receiver = receiver
-            }
+            getIcmfRemoteSpecialTxExtension(configuration)?.receiver = receiver
         }
     }
 
