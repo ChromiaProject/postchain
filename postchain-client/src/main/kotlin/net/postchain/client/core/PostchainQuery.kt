bf8c3912c72714f3d850fe0a3a9d93dc87126fc3
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-10-11 16:26:34 +0200

    Fix doc

--- postchain-client/src/main/kotlin/net/postchain/client/core/PostchainQuery.kt
+++ postchain-client/src/main/kotlin/net/postchain/client/core/PostchainQuery.kt
@@ -7,10 +7,12 @@ import net.postchain.gtv.GtvDictionary
  * Query a Postchain blockchain.
  */
 interface PostchainQuery {
-/**
- * Perform a query
- * @param name name of the query
- * @param gtv query arguments
- * [Note] Arguments must be provided as a GtvDictionary
+    /**
+     * Perform a query.
+     *
+     * @param name name of the query
+     * @param gtv query arguments, must be provided as a GtvDictionary
+     * @return query result
+     */
     fun querySync(name: String, gtv: Gtv = GtvDictionary.build(mapOf())): Gtv
 }
