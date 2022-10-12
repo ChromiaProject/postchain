d37559dddfcb4232a3192746551125b8817a2297
Eugene Tykulov <eugene.tykulov@chromaway.com>
2022-10-12 17:02:39 +0300

    Testmode feature has been moved from BaseDirectoryDataSource to TestmodeDirectoryDataSource

--- postchain-base/src/main/kotlin/net/postchain/managed/BaseDirectoryDataSource.kt
+++ postchain-base/src/main/kotlin/net/postchain/managed/BaseDirectoryDataSource.kt
@@ -4,14 +4,12 @@ import net.postchain.common.BlockchainRid
 import net.postchain.config.app.AppConfig
 import net.postchain.containers.bpm.ContainerResourceLimits
 import net.postchain.containers.bpm.ContainerResourceLimits.ResourceLimit
-import net.postchain.containers.infra.ContainerNodeConfig
 import net.postchain.gtv.GtvFactory
 import net.postchain.managed.query.QueryRunner
 
-class BaseDirectoryDataSource(
+open class BaseDirectoryDataSource(
         queryRunner: QueryRunner,
         appConfig: AppConfig,
-        private val containerNodeConfig: ContainerNodeConfig,
 ) : BaseManagedNodeDataSource(queryRunner, appConfig), DirectoryDataSource {
 
     override fun getContainersToRun(): List<String>? {
@@ -33,39 +31,26 @@ class BaseDirectoryDataSource(
     }
 
     override fun getContainerForBlockchain(brid: BlockchainRid): String {
-        return if (containerNodeConfig.testmode) {
-            val short = brid.toHex().uppercase().take(8)
-            containerNodeConfig.testmodeDappsContainers[short] ?: "cont0"
+        return if (nmApiVersion >= 3) {
+            query(
+                    "nm_get_container_for_blockchain",
+                    buildArgs("blockchain_rid" to GtvFactory.gtv(brid.data))
+            ).asString()
         } else {
-            if (nmApiVersion >= 3) {
-                query(
-                        "nm_get_container_for_blockchain",
-                        buildArgs("blockchain_rid" to GtvFactory.gtv(brid.data))
-                ).asString()
-            } else {
-                throw Exception("Directory1 v.{$nmApiVersion} doesn't support 'nm_get_container_for_blockchain' query")
-            }
+            throw Exception("Directory1 v.{$nmApiVersion} doesn't support 'nm_get_container_for_blockchain' query")
         }
     }
 
     // TODO: [et]: directory vs containerId?
     override fun getResourceLimitForContainer(containerId: String): ContainerResourceLimits {
-        return if (containerNodeConfig.testmode) {
-            ContainerResourceLimits.fromValues(
-                    containerNodeConfig.testmodeResourceLimitsRAM,
-                    containerNodeConfig.testmodeResourceLimitsCPU,
-                    containerNodeConfig.testmodeResourceLimitsSTORAGE
-            )
-        } else {
-            val resourceLimits = query(
-                    "nm_get_container_limits",
-                    buildArgs("name" to GtvFactory.gtv(containerId))
-            ).asDict()
-                    .map { ResourceLimit.valueOf(it.key.uppercase()) to it.value.asInteger() }
-                    .toMap()
+        val resourceLimits = query(
+                "nm_get_container_limits",
+                buildArgs("name" to GtvFactory.gtv(containerId))
+        ).asDict()
+                .map { ResourceLimit.valueOf(it.key.uppercase()) to it.value.asInteger() }
+                .toMap()
 
-            ContainerResourceLimits(resourceLimits)
-        }
+        return ContainerResourceLimits(resourceLimits)
     }
 
     override fun setLimitsForContainer(containerId: String, ramLimit: Long, cpuQuota: Long) {
