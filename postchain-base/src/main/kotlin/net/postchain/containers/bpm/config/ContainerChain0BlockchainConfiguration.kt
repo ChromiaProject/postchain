ac9d11ff3e5faf396f781eeb96d164104b41a2e8
Robert Wideberg <robert.wideberg@chromaway.com>
2022-10-27 21:54:12 +0200

    Align container managed mode with regular managed mode implementation

--- postchain-base/src/main/kotlin/net/postchain/containers/bpm/config/ContainerChain0BlockchainConfiguration.kt
+++ postchain-base/src/main/kotlin/net/postchain/containers/bpm/config/ContainerChain0BlockchainConfiguration.kt
@@ -5,15 +5,16 @@ import net.postchain.containers.infra.ContainerNodeConfig
 import net.postchain.core.BlockchainConfiguration
 import net.postchain.gtx.GTXBlockchainConfiguration
 import net.postchain.gtx.GTXModule
+import net.postchain.gtx.GTXModuleAwareness
 import net.postchain.managed.TestmodeDirectoryDataSource
 import net.postchain.managed.config.ManagedDataSourceAwareness
 import net.postchain.managed.query.QueryRunnerFactory.createChain0QueryRunner
 
 class ContainerChain0BlockchainConfiguration(
         configuration: GTXBlockchainConfiguration,
-        module: GTXModule, appConfig: AppConfig,
+        override val module: GTXModule, appConfig: AppConfig,
         val containerNodeConfig: ContainerNodeConfig,
-) : BlockchainConfiguration by configuration, ManagedDataSourceAwareness {
+) : BlockchainConfiguration by configuration, ManagedDataSourceAwareness, GTXModuleAwareness {
 
     override val dataSource = TestmodeDirectoryDataSource(
             createChain0QueryRunner(module, appConfig), appConfig, containerNodeConfig)
