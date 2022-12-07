b7da7f1060329257cf6e5efdafe7063e5322a3e3
Eugene Tykulov <eugene.tykulov@chromaway.com>
2022-12-07 12:59:24 +0200

    Code cleanup

--- postchain-devtools/src/test/kotlin/net/postchain/integrationtest/managedmode/TestPeerInfos.kt
+++ postchain-devtools/src/test/kotlin/net/postchain/integrationtest/managedmode/TestPeerInfos.kt
@@ -20,18 +20,6 @@ class TestPeerInfos {
                 9871,
                 "031B84C5567B126440995D3ED5AABA0565D71E1834604819FF9C17F5E9D5DD078F".hexStringToByteArray()
         )
-
-        val peerInfo2 = PeerInfo(
-                "127.0.0.1",
-                9872,
-                "03B2EF623E7EC933C478135D1763853CBB91FC31BA909AEC1411CA253FDCC1AC94".hexStringToByteArray()
-        )
-
-        val peerInfo3 = PeerInfo(
-                "127.0.0.1",
-                9873,
-                "0203C6150397F7E4197FF784A8D74357EF20DAF1D09D823FFF8D3FC9150CBAE85D".hexStringToByteArray()
-        )
     }
 
 }
