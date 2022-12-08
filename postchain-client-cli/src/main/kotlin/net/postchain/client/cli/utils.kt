3c5486541ef226160ec71663b5fe2c23267491c7
Joar Svartholm <joar.svartholm@chromaway.com>
2022-12-08 10:10:53 +0100

    Move gtv parsing to gtv module

--- postchain-client-cli/src/main/kotlin/net/postchain/client/cli/utils.kt
+++ postchain-client-cli/src/main/kotlin/net/postchain/client/cli/utils.kt
@@ -7,30 +7,9 @@ import net.postchain.gtv.GtvInteger
 import net.postchain.gtv.GtvNull
 import net.postchain.gtv.GtvString
 import kotlin.text.Typography.quote
+import net.postchain.gtv.parse.GtvParser
 
+@Deprecated("Use GtvParser.parse in stead")
 fun encodeArg(arg: String): Gtv {
-    return when {
-        arg == "null" -> GtvNull
-        arg.startsWith("x\"") && arg.endsWith(quote) -> encodeByteArray(arg.substring(1))
-        arg.startsWith("[") && arg.endsWith("]") -> encodeArray(arg.trim('[', ']'))
-        arg.startsWith("{") && arg.endsWith("}") -> encodeDict(arg.trim('{', '}'))
-        else -> arg.toLongOrNull()?.let(::GtvInteger) ?: GtvString(arg.trim(quote))
-    }
-}
-
-private fun encodeByteArray(arg: String): Gtv {
-    val bytearray = arg.trim(quote)
-    return try {
-        GtvFactory.gtv(bytearray.hexStringToByteArray())
-    } catch (e: IllegalArgumentException) {
-        GtvFactory.gtv(bytearray.toByteArray())
-    }
-}
-
-private fun encodeArray(arg: String) = GtvFactory.gtv(arg.split(",").map { encodeArg(it) })
-
-private fun encodeDict(arg: String): Gtv {
-    val pairs = arg.split(",").map { it.split("->", limit = 2) }
-    if (pairs.any { it.size < 2 }) throw IllegalArgumentException("Wrong format. Expected dict $arg to contain key=value pairs")
-    return GtvFactory.gtv(pairs.associateBy({ it[0] }, { encodeArg(it[1]) }))
+    return GtvParser.parse(arg)
 }
