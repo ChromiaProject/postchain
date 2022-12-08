39fee5a4da3f3ebf08c5a7921ba90194db628c03
Joar Svartholm <joar.svartholm@chromaway.com>
2022-12-08 11:11:31 +0100

    Add a start-command for starting postchain with a specific configuration

--- postchain-cli/src/main/kotlin/net/postchain/app.kt
+++ postchain-cli/src/main/kotlin/net/postchain/app.kt
@@ -22,6 +22,7 @@ import net.postchain.cli.CommandRemoveConfiguration
 import net.postchain.cli.CommandRunNode
 import net.postchain.cli.CommandRunNodeAuto
 import net.postchain.cli.CommandRunServer
+import net.postchain.cli.CommandStart
 import net.postchain.cli.CommandWaitDb
 import net.postchain.cli.CommandWipeDb
 import java.io.File
@@ -57,6 +58,7 @@ fun main(args: Array<String>) {
                     CommandRunNode(),
                     CommandRunNodeAuto(),
                     CommandRunServer(),
+                    CommandStart(),
                     CommandWaitDb(),
                     CommandWipeDb(),
             )
