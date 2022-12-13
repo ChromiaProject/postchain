d3b1553d2b18426a909d981a6c08d519c10630b6
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-12-13 17:18:15 +0100

    Fix merge conflict

--- postchain-base/src/main/kotlin/net/postchain/gtx/GTXBlockchainConfigurationFactory.kt
+++ postchain-base/src/main/kotlin/net/postchain/gtx/GTXBlockchainConfigurationFactory.kt
@@ -2,17 +2,13 @@ package net.postchain.gtx
 
 import net.postchain.base.configuration.BlockchainConfigurationData
 import net.postchain.common.BlockchainRid
-import net.postchain.common.data.Hash
 import net.postchain.common.exception.UserMistake
 import net.postchain.core.BlockchainConfigurationFactory
 import net.postchain.core.BlockchainContext
 import net.postchain.core.EContext
 import net.postchain.crypto.CryptoSystem
 import net.postchain.crypto.SigMaker
-import net.postchain.crypto.SigMaker
-import net.postchain.crypto.Signature
 import net.postchain.gtv.Gtv
-import net.postchain.gtv.GtvEncoder
 import net.postchain.gtv.mapper.toObject
 
 /**
@@ -24,12 +20,7 @@ open class GTXBlockchainConfigurationFactory : BlockchainConfigurationFactory {
     companion object {
         fun validateConfiguration(config: Gtv, blockchainRid: BlockchainRid) {
             val configurationData = try {
-                BlockchainConfigurationData.fromRaw(
-                        GtvEncoder.encodeGtv(config), blockchainRid, -1, -1L, ByteArray(0),
-                        object : SigMaker {
-                            override fun signMessage(msg: ByteArray): Signature = throw NotImplementedError()
-                            override fun signDigest(digest: Hash): Signature = throw NotImplementedError()
-                        })
+                config.toObject<BlockchainConfigurationData>()
             } catch (e: IllegalArgumentException) {
                 throw UserMistake("Unable to parse configuration: ${e.message}", e)
             }
@@ -79,47 +70,6 @@ open class GTXBlockchainConfigurationFactory : BlockchainConfigurationFactory {
                 cryptoSystem,
                 partialContext,
                 blockSigMaker,
-                createGtxModule(effectiveBRID, configurationData, eContext)
-        )
-    }
-
-        private fun makeGtxModule(blockchainRID: BlockchainRid, data: BlockchainConfigurationData): GTXModule {
-            val gtxConfig = data.gtx?.toObject() ?: GtxConfigurationData.default
-            val list = gtxConfig.modules.distinct()
-            if (list.isEmpty()) {
-                throw UserMistake("Missing GTX module in config. expected property 'blockchain.<chainId>.gtx.modules'")
-            }
-
-            fun makeModule(name: String): GTXModule {
-                // Alias function for GTX modules that have been moved
-                val className = when (name) {
-                    "PatchOpsGTXModule" -> PatchOpsGTXModule::class.qualifiedName
-                    else -> name
-                }
-
-                val moduleClass = Class.forName(className).getConstructor()
-                return when (val instance = moduleClass.newInstance()) {
-                    is GTXModule -> instance
-                    is GTXModuleFactory -> instance.makeModule(data.rawConfig, blockchainRID) //TODO
-                    else -> throw UserMistake("Module class not recognized")
-                }
-            }
-
-            return if (list.size == 1) {
-                makeModule(list[0])
-            } else {
-                val moduleList = list.map(::makeModule)
-                CompositeGTXModule(moduleList.toTypedArray(), gtxConfig.allowOverrides)
-            }
-        }
-    }
-
-    override fun makeBlockchainConfiguration(configurationData: Any, eContext: EContext, cryptoSystem: CryptoSystem): GTXBlockchainConfiguration {
-        val cfData = configurationData as BlockchainConfigurationData
-        val effectiveBRID = cfData.historicBrid ?: configurationData.context.blockchainRID
-        return GTXBlockchainConfiguration(
-                cfData,
-                cryptoSystem,
                 createGtxModule(effectiveBRID, configurationData, eContext)
         )
     }
