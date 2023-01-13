2a3cb896326e8662486f0991b16d952425f1cdaa
Mikael Ståldal <mikael.staldal@chromaway.com>
2023-01-13 16:57:09 +0000

    Refactor keygen command in preparation for making library function to be reused by PMC and Chromia CLI

--- postchain-cli/src/main/kotlin/net/postchain/cli/CommandKeygen.kt
+++ postchain-cli/src/main/kotlin/net/postchain/cli/CommandKeygen.kt
@@ -5,7 +5,11 @@ package net.postchain.cli
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.file
 import net.postchain.common.toHex
+import net.postchain.crypto.KeyPair
+import net.postchain.crypto.PrivKey
+import net.postchain.crypto.PubKey
 import net.postchain.crypto.Secp256K1CryptoSystem
 import net.postchain.crypto.secp256k1_derivePubKey
 import org.bitcoinj.crypto.MnemonicCode
@@ -13,56 +17,66 @@ import java.io.File
 import java.io.FileOutputStream
 import java.util.*
 
-
 class CommandKeygen : CliktCommand(name = "keygen", help = "Generates public/private key pair") {
 
     private val wordList by option(
-        "-m", "--mnemonic",
-        help = """
+            "-m", "--mnemonic",
+            help = """
             Mnemonic word list, words separated by space, e.g:
                 "lift employ roast rotate liar holiday sun fever output magnet...""
         """.trimIndent()
     )
-        .default("")
+            .default("")
 
     private val file by option("-s", "--save", help = "File to save the generated keypair in")
+            .file(canBeDir = false)
 
     /**
      * Cryptographic key generator. Will generate a pair of public and private keys and print to stdout.
      */
     override fun run() {
-        val cs = Secp256K1CryptoSystem()
-        var privKey = cs.generatePrivKey().data
-        val mnemonicInstance = MnemonicCode.INSTANCE
-        var mnemonic = mnemonicInstance.toMnemonic(privKey).joinToString(" ")
-        if (wordList.isNotEmpty()) {
-            val words = wordList.split(" ")
-            mnemonicInstance.check(words)
-            mnemonic = wordList
-            privKey = mnemonicInstance.toEntropy(words)
-        }
+        val (keyPair, mnemonic) = generateSecp256k1KeyPairWithMnemonic(wordList)
 
-        val pubKey = secp256k1_derivePubKey(privKey)
-
-        file?.let { fileName ->
-            if (!File(fileName).parentFile.exists()) File(fileName).parentFile.mkdirs()
-            val properties = Properties()
-            properties["privkey"] = privKey.toHex()
-            properties["pubkey"] = pubKey.toHex()
-
-            FileOutputStream(fileName).use { fs ->
-                properties.store(fs, "Keypair generated using secp256k1")
-                fs.flush()
-            }
+        file?.let {
+            saveSecp256k1KeyPair(keyPair, it)
         }
         println(
-            """
-            |privkey:   ${privKey.toHex()}
-            |pubkey:    ${pubKey.toHex()}
+                """
+            |privkey:   ${keyPair.privKey.data.toHex()}
+            |pubkey:    ${keyPair.pubKey.data.toHex()}
             |mnemonic:  $mnemonic 
         """.trimMargin()
         )
     }
+}
 
+private fun generateSecp256k1KeyPairWithMnemonic(wordList: String): Pair<KeyPair, String> {
+    val cs = Secp256K1CryptoSystem()
 
+    var privKey = cs.generatePrivKey().data
+    val mnemonicInstance = MnemonicCode.INSTANCE
+    var mnemonic = mnemonicInstance.toMnemonic(privKey).joinToString(" ")
+    if (wordList.isNotEmpty()) {
+        val words = wordList.split(" ")
+        mnemonicInstance.check(words)
+        mnemonic = wordList
+        privKey = mnemonicInstance.toEntropy(words)
+    }
+
+    val pubKey = secp256k1_derivePubKey(privKey)
+
+    val keyPair = KeyPair(PubKey(pubKey), PrivKey(privKey))
+    return keyPair to mnemonic
+}
+
+private fun saveSecp256k1KeyPair(keyPair: KeyPair, file: File) {
+    if (!file.parentFile.exists()) file.parentFile.mkdirs()
+    val properties = Properties()
+    properties["privkey"] = keyPair.privKey.data.toHex()
+    properties["pubkey"] = keyPair.pubKey.data.toHex()
+
+    FileOutputStream(file).use { fs ->
+        properties.store(fs, "Keypair generated using secp256k1")
+        fs.flush()
+    }
 }
