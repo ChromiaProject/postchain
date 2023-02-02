a74b3a78143cfd7b2fa92918f0933e525c41869a
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-02-02 16:31:21 +0200

    Old comments deleted

--- postchain-base/src/main/kotlin/net/postchain/containers/bpm/docker/DockerClientFactory.kt
+++ postchain-base/src/main/kotlin/net/postchain/containers/bpm/docker/DockerClientFactory.kt
@@ -5,25 +5,6 @@ import org.mandas.docker.client.builder.jersey.JerseyDockerClientBuilder
 
 object DockerClientFactory {
 
-    /**
-     * Docker-in-Docker attempts on local machine:
-     * DockerClient.builder().uri("http://172.26.32.1:2375") // .uri("unix:///var/run/docker.sock")
-     */
-
-    /**
-     * Docker-in-Docker on Bitbucket Pipelines
-     *
-     * We set BUFFERED transfer encoding rather than CHUNKED because it sends content-length header
-     * while CHUNKED doesn't. By default, ApacheConnectorProvider uses CHUNKED mode.
-     *
-     * Docker authZ plugin requires content-length header for requests with a body
-     * to allow requests to be proceeded by docker daemon.
-     *
-     * See
-     *  - https://jira.atlassian.com/browse/BCLOUD-15844
-     *  - https://github.com/spotify/docker-client/pull/1021
-     *  - https://community.atlassian.com/t5/Bitbucket-questions/create-container-authorization-denied-by-plugin-pipelines/qaq-p/731364
-     */
     fun create(): DockerClient = JerseyDockerClientBuilder().fromEnv().build()
 
 }
