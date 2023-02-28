6a62fe6cb39f6a7d4a893848a5475cb47e8c649c
Robert Wideberg <robert.wideberg@chromaway.com>
2023-02-28 11:49:51 +0100

    Refactoring test code

--- postchain-base/src/test/kotlin/net/postchain/base/BaseBlockHeaderTestMerkleProofTest.kt
+++ postchain-base/src/test/kotlin/net/postchain/base/BaseBlockHeaderTestMerkleProofTest.kt
@@ -7,7 +7,7 @@ import net.postchain.gtv.merkle.GtvMerkleHashCalculator
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-class BaseBlockHeaderTestMerkleProofTest: BaseBlockHeaderTestSuper() {
+class BaseBlockHeaderTestMerkleProofTest {
     val blockchainRID = BlockchainRid.ZERO_RID
 
     val cryptoSystem = Secp256K1CryptoSystem()
@@ -18,49 +18,49 @@ class BaseBlockHeaderTestMerkleProofTest: BaseBlockHeaderTestSuper() {
 
     @Test
     fun proof_test_first() {
-        val header0 = createHeader(blockchainRID,2L, 0, prevBlockRid, 0, merkeHashCalculator)
+        val header0 = createBlockHeader(blockchainRID, 2L, 0, prevBlockRid, 0, merkeHashCalculator)
         val decodedHeader0 = BaseBlockHeader(header0.rawData, merkeHashCalculator)
 
         val wrappedTxList = arrayOf(
-            wrappedProveTx,
-            WrappedByteArray.fromHex("11112222"),
-            WrappedByteArray.fromHex("22223333"),
-            WrappedByteArray.fromHex("33334444")
-            )
+                wrappedProveTx,
+                WrappedByteArray.fromHex("11112222"),
+                WrappedByteArray.fromHex("22223333"),
+                WrappedByteArray.fromHex("33334444")
+        )
         val ret = decodedHeader0.merkleProofTree(wrappedProveTx, wrappedTxList)
 
-        assertEquals(ret.first,0L)
+        assertEquals(ret.first, 0L)
     }
 
     @Test
     fun proof_test_last() {
-        val header0 = createHeader(blockchainRID,2L, 0, prevBlockRid, 0, merkeHashCalculator)
+        val header0 = createBlockHeader(blockchainRID, 2L, 0, prevBlockRid, 0, merkeHashCalculator)
         val decodedHeader0 = BaseBlockHeader(header0.rawData, merkeHashCalculator)
 
         val wrappedTxList = arrayOf(
-            WrappedByteArray.fromHex("11112222"),
-            WrappedByteArray.fromHex("22223333"),
-            WrappedByteArray.fromHex("33334444"),
-            wrappedProveTx
+                WrappedByteArray.fromHex("11112222"),
+                WrappedByteArray.fromHex("22223333"),
+                WrappedByteArray.fromHex("33334444"),
+                wrappedProveTx
         )
         val ret = decodedHeader0.merkleProofTree(wrappedProveTx, wrappedTxList)
 
-        assertEquals(ret.first,3L)
+        assertEquals(ret.first, 3L)
     }
 
     @Test
     fun proof_test_mid() {
-        val header0 = createHeader(blockchainRID,2L, 0, prevBlockRid, 0, merkeHashCalculator)
+        val header0 = createBlockHeader(blockchainRID, 2L, 0, prevBlockRid, 0, merkeHashCalculator)
         val decodedHeader0 = BaseBlockHeader(header0.rawData, merkeHashCalculator)
 
         val wrappedTxList = arrayOf(
-            WrappedByteArray.fromHex("11112222"),
-            WrappedByteArray.fromHex("22223333"),
-            wrappedProveTx,
-            WrappedByteArray.fromHex("33334444")
+                WrappedByteArray.fromHex("11112222"),
+                WrappedByteArray.fromHex("22223333"),
+                wrappedProveTx,
+                WrappedByteArray.fromHex("33334444")
         )
         val ret = decodedHeader0.merkleProofTree(wrappedProveTx, wrappedTxList)
 
-        assertEquals(ret.first,2L)
+        assertEquals(ret.first, 2L)
     }
 }
