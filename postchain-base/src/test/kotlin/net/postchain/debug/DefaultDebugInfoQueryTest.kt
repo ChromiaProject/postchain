ffd52a80d07176bd69d339b033eec19a595ed622
Mikael Ståldal <mikael.staldal@chromaway.com>
2023-08-28 10:59:09 +0000

    POS-893 Support generic queries for REST API _debug endpoint

--- postchain-base/src/test/kotlin/net/postchain/debug/DefaultDebugInfoQueryTest.kt
+++ postchain-base/src/test/kotlin/net/postchain/debug/DefaultDebugInfoQueryTest.kt
@@ -1,11 +1,17 @@
 package net.postchain.debug
 
+import assertk.assertFailure
 import net.postchain.api.rest.controller.DefaultDebugInfoQuery
 import net.postchain.api.rest.controller.DisabledDebugInfoQuery
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.assertEquals
 import assertk.assertThat
-
+import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import assertk.assertions.isTrue
+import assertk.assertions.messageContains
+import net.postchain.common.BlockchainRid
+import net.postchain.common.exception.UserMistake
+import net.postchain.common.hexStringToByteArray
 
 class DefaultDebugInfoQueryTest {
 
@@ -18,8 +24,8 @@ class DefaultDebugInfoQueryTest {
         val actual = json.asJsonObject
 
         // Asserts
-        assertEquals(1, actual.size())
-        assertThat(actual.has("blockchain"))
+        assertThat(actual.size()).isEqualTo(1)
+        assertThat(actual.has("blockchain")).isTrue()
     }
 
     @Test
@@ -35,25 +41,79 @@ class DefaultDebugInfoQueryTest {
         val actual = json.asJsonObject
 
         // Asserts
-        assertEquals(3, actual.size())
-        assertEquals("1.1.1", actual.get(DiagnosticProperty.VERSION.prettyName).asString)
-        assertEquals("my-container", actual.get(DiagnosticProperty.CONTAINER_NAME.prettyName).asString)
+        assertThat(actual.size()).isEqualTo(3)
+        assertThat(actual.get(DiagnosticProperty.VERSION.prettyName).asString).isEqualTo("1.1.1")
+        assertThat(actual.get(DiagnosticProperty.CONTAINER_NAME.prettyName).asString).isEqualTo("my-container")
     }
 
     @Test
-    fun testError_When_Subquery_IsGiven() {
+    fun testSubqueryWithBlockStatsQuery() {
+        val debugContext = JsonNodeDiagnosticContext(DiagnosticProperty.VERSION withValue "1.1.1")
+        debugContext.blockchainData(BlockchainRid("4EF13F43A95760D17A267222C61AEEF11542DC62FB8D151AECEAC919F660DD73".hexStringToByteArray()))
+        val sut = DefaultDebugInfoQuery(debugContext)
+
+        // Actions
+        val json = sut.queryDebugInfo("block-statistics")
+
+        // Asserts
+        val actual = json.asJsonArray
+        assertThat(actual.size()).isEqualTo(1)
+        assertThat(actual.get(0).asJsonObject.get(DiagnosticProperty.BLOCKCHAIN_RID.prettyName).asString).isEqualTo("4EF13F43A95760D17A267222C61AEEF11542DC62FB8D151AECEAC919F660DD73")
+        assertThat(actual.get(0).asJsonObject.get(DiagnosticProperty.BLOCK_STATS.prettyName).isJsonArray).isTrue()
+    }
+
+    @Test
+    fun testSubqueryWithJsonPath() {
         val debugContext = JsonNodeDiagnosticContext(
                 DiagnosticProperty.VERSION withValue "1.1.1",
                 DiagnosticProperty.CONTAINER_NAME withLazyValue { "my-container" })
         val sut = DefaultDebugInfoQuery(debugContext)
 
         // Actions
-        val json = sut.queryDebugInfo("subquery")
-        val actual = json.asJsonObject
+        val json = sut.queryDebugInfo("$.container-name")
+        val actual = json.asJsonPrimitive
 
         // Asserts
-        assertEquals(1, actual.size())
-        assertThat(actual.has("Error"))
+        assertThat(actual.asString).isEqualTo("my-container")
+    }
+
+    @Test
+    fun testSubqueryWithIllegalJsonPath() {
+        val debugContext = JsonNodeDiagnosticContext(
+                DiagnosticProperty.VERSION withValue "1.1.1",
+                DiagnosticProperty.CONTAINER_NAME withLazyValue { "my-container" })
+        val sut = DefaultDebugInfoQuery(debugContext)
+
+        // Actions
+        assertFailure {
+            sut.queryDebugInfo("[")
+        }.isInstanceOf(UserMistake::class)
+    }
+
+    @Test
+    fun testSubqueryWithTooLongJsonPath() {
+        val debugContext = JsonNodeDiagnosticContext(
+                DiagnosticProperty.VERSION withValue "1.1.1",
+                DiagnosticProperty.CONTAINER_NAME withLazyValue { "my-container" })
+        val sut = DefaultDebugInfoQuery(debugContext)
+
+        // Actions
+        assertFailure {
+            sut.queryDebugInfo("x".repeat(2000))
+        }.isInstanceOf(UserMistake::class).messageContains("cannot be longer than")
+    }
+
+    @Test
+    fun testSubqueryWithNotFoundPath() {
+        val debugContext = JsonNodeDiagnosticContext(
+                DiagnosticProperty.VERSION withValue "1.1.1",
+                DiagnosticProperty.CONTAINER_NAME withLazyValue { "my-container" })
+        val sut = DefaultDebugInfoQuery(debugContext)
+
+        // Actions
+        assertFailure {
+            sut.queryDebugInfo("$.bogus")
+        }.isInstanceOf(UserMistake::class).messageContains("No results for path")
     }
 
     @Test
@@ -65,8 +125,7 @@ class DefaultDebugInfoQueryTest {
         val actual = json.asJsonObject
 
         // Asserts
-        assertEquals(1, actual.size())
-        assertThat(actual.has("error"))
+        assertThat(actual.size()).isEqualTo(1)
+        assertThat(actual.has("error")).isTrue()
     }
-
 }
