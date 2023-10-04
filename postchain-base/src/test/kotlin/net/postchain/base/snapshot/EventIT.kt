2d59dffbe345c3bfdf944142c7a703c6d5dcbc7e
Ha ĐANG <dvietha@gmail.com>
2023-10-04 14:48:41 +0700

    POS-974: Run test for snapshot with actual db touch

---
+++ postchain-base/src/test/kotlin/net/postchain/base/snapshot/EventIT.kt
@@ -0,0 +1,87 @@
+package net.postchain.base.snapshot
+
+import net.postchain.StorageBuilder
+import net.postchain.base.BaseBlockEContext
+import net.postchain.base.BaseTxEContext
+import net.postchain.base.TxEventSink
+import net.postchain.base.data.DatabaseAccess
+import net.postchain.base.data.testDbConfig
+import net.postchain.base.withWriteConnection
+import net.postchain.common.BlockchainRid
+import net.postchain.common.data.Hash
+import net.postchain.common.toHex
+import net.postchain.config.app.AppConfig
+import net.postchain.core.TxEContext
+import net.postchain.crypto.KeyPair
+import net.postchain.crypto.Secp256K1CryptoSystem
+import net.postchain.crypto.devtools.KeyPairHelper
+import net.postchain.gtv.Gtv
+import net.postchain.gtv.GtvFactory
+import net.postchain.gtx.GTXTransaction
+import net.postchain.gtx.GTXTransactionFactory
+import net.postchain.gtx.GtxBuilder
+import net.postchain.gtx.GtxNop
+import net.postchain.gtx.StandardOpsGTXModule
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+
+class EventIT: SnapshotBaseIT() {
+    private val cs = Secp256K1CryptoSystem()
+    private val appConfig: AppConfig = testDbConfig("database_event_tree")
+
+    @Test
+    fun testEventTree() {
+        val storage = StorageBuilder.buildStorage(appConfig, wipeDatabase = true)
+        val chainId = 0L
+
+        withWriteConnection(storage, chainId) { ctx ->
+            val db = DatabaseAccess.of(ctx)
+            db.initializeBlockchain(ctx, BlockchainRid.ZERO_RID)
+            db.apply {
+                createPageTable(ctx, "${PREFIX}_event")
+                createEventLeafTable(ctx, PREFIX)
+            }
+            val dummyEventSink = object : TxEventSink {
+                override fun processEmittedEvent(ctxt: TxEContext, type: String, data: Gtv) {
+                    return
+                }
+            }
+            val bctx = BaseBlockEContext(ctx, 0, 1, 10, mapOf(), dummyEventSink)
+            db.insertBlock(ctx, 1)
+            val signers = listOf(KeyPairHelper.pubKey(0))
+            val sigMaker = cs.buildSigMaker(KeyPair(KeyPairHelper.pubKey(0), KeyPairHelper.privKey(0)))
+            val factory = GTXTransactionFactory(BlockchainRid.ZERO_RID, StandardOpsGTXModule(), cs)
+            val gtxData = GtxBuilder(BlockchainRid.ZERO_RID, signers, cs)
+                    .addOperation(GtxNop.OP_NAME, GtvFactory.gtv(42))
+                    .finish().sign(sigMaker).buildGtx().encode()
+            val tx = factory.decodeTransaction(gtxData) as GTXTransaction
+            val txIid = db.insertTransaction(bctx, tx, 1)
+            val txEContext = BaseTxEContext(bctx, txIid, tx)
+            val event = EventPageStore(bctx, levelsPerPage, ds, PREFIX)
+
+            val blockHeight = 1L
+            val leafs = arrayListOf<Hash>()
+            for (i in 1..129) {
+                val data = BigInteger.valueOf(i.toLong()).toByteArray()
+                val hash = ds.digest(data)
+                db.insertEvent(txEContext, PREFIX, blockHeight, leafs.size.toLong(), hash, data)
+                leafs.add(hash)
+            }
+
+            leafs.forEach { hash ->
+                val eventInfo = db.getEvent(ctx, PREFIX, hash)
+                assertEquals(hash.toHex(), eventInfo!!.hash.toHex())
+            }
+
+            val root = event.writeEventTree(blockHeight, leafs)
+            arrayOf(0, 1, 36, 88, 12, 128).forEach {
+                val proofs = event.getMerkleProof(blockHeight, it.toLong())
+                val expected = getMerkleRoot(proofs, it, leafs[it])
+                assertEquals(expected.toHex(), root.toHex())
+            }
+
+            true
+        }
+    }
+}
