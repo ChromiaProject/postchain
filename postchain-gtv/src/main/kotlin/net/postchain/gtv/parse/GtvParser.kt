f587f84c22bed61113c5c2edf187bb5147dd0120
Robert Wideberg <robert.wideberg@chromaway.com>
2023-10-10 16:40:51 +0200

    Require escaping of equal sign when used inside string values in dicts

--- postchain-gtv/src/main/kotlin/net/postchain/gtv/parse/GtvParser.kt
+++ postchain-gtv/src/main/kotlin/net/postchain/gtv/parse/GtvParser.kt
@@ -9,7 +9,10 @@ import net.postchain.gtv.GtvNull
 import net.postchain.gtv.GtvString
 
 object GtvParser {
-    fun parse(str: String): Gtv {
+
+    private val UNESCAPED_EQUAL_SIGN_PATTERN = "(?<!\\\\)=".toRegex()
+
+    fun parse(str: String, isDictValue: Boolean = false): Gtv {
         val s = str.trim()
         return when {
             s == "null" -> GtvNull
@@ -17,10 +20,12 @@ object GtvParser {
             s.startsWith("[") && s.endsWith("]") -> parseArray(s.removeSurrounding("[", "]"))
             s.startsWith("{") && s.endsWith("}") -> parseDict(s.removeSurrounding("{", "}"))
             s.endsWith("L") -> {
-                s.substring(0, s.length - 1).toBigIntegerOrNull()?.let { GtvBigInteger(it) } ?: GtvString(s)
+                s.substring(0, s.length - 1).toBigIntegerOrNull()?.let { GtvBigInteger(it) }
+                        ?: if (isDictValue) parseDictValueString(s) else GtvString(s)
             }
 
-            else -> s.toLongOrNull()?.let(::GtvInteger) ?: GtvString(s.trim(Typography.quote))
+            else -> s.toLongOrNull()?.let(::GtvInteger)
+                    ?: if (isDictValue) parseDictValueString(s) else GtvString(s.trim(Typography.quote))
         }
     }
 
@@ -32,8 +37,17 @@ object GtvParser {
     private fun parseDict(str: String) = gtv(splitArray(str).associate {
         if (!it.contains("=")) throw IllegalArgumentException("$it must be a key-value pair separated by \"=\"")
         val (key, value) = it.split("=", limit = 2)
-        key.trim() to parse(value)
+        key.trim() to parse(value, true)
     })
+
+    private fun parseDictValueString(arg: String): Gtv {
+        val dictValueString = arg.trim(Typography.quote)
+        if (dictValueString.contains(UNESCAPED_EQUAL_SIGN_PATTERN)) {
+            throw IllegalArgumentException("\"=\" characters inside dict string values must be escaped by a preceding" +
+                    " \"\\\" character. Ensure that you are using the correct separator \",\" for dict key-value pairs.")
+        }
+        return gtv(dictValueString.replace("\\=", "="))
+    }
 
     private fun parseArray(str: String) = gtv(splitArray(str).map { parse(it) })
 
