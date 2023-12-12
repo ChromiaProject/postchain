c89ad07a3008805d29824af98a69d8dcb5b8ad4d
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-12-12 17:00:18 +0200

    POS-1032: Review comments

--- postchain-base/src/main/kotlin/net/postchain/containers/bpm/DefaultPostchainContainer.kt
+++ postchain-base/src/main/kotlin/net/postchain/containers/bpm/DefaultPostchainContainer.kt
@@ -117,7 +117,7 @@ class DefaultPostchainContainer(
     }
 
     override fun isIdle(): Boolean {
-        return processes.isEmpty() && (System.currentTimeMillis() - lastUpdated > 5 * 60 * 1000)
+        return processes.isEmpty() && (clock.millis() - lastUpdated > 5 * 60 * 1000)
     }
 
     override fun isSubnodeHealthy() = subnodeAdminClient.isSubnodeHealthy()
