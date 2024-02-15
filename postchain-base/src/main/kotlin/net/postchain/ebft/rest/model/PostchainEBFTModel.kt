ddd59d0605e592e2424d6661693449431fa96559
he huang <he.huang@chromaway.com>
2024-02-15 13:44:30 +0100

    add opentelemetry manual instrument

--- postchain-base/src/main/kotlin/net/postchain/ebft/rest/model/PostchainEBFTModel.kt
+++ postchain-base/src/main/kotlin/net/postchain/ebft/rest/model/PostchainEBFTModel.kt
@@ -4,6 +4,9 @@ package net.postchain.ebft.rest.model
 
 import io.micrometer.core.instrument.Metrics
 import io.micrometer.core.instrument.Timer
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.api.trace.Tracer
+import io.opentelemetry.context.Context
 import net.postchain.PostchainContext
 import net.postchain.api.rest.controller.DuplicateTnxException
 import net.postchain.api.rest.controller.InvalidTnxException
@@ -17,6 +20,7 @@ import net.postchain.core.Storage
 import net.postchain.core.TransactionQueue
 import net.postchain.core.block.BlockQueries
 import net.postchain.debug.DiagnosticData
+import net.postchain.traces.OpenTelemetryFactory
 
 class PostchainEBFTModel(
         blockchainConfiguration: BlockchainConfiguration,
@@ -31,37 +35,62 @@ class PostchainEBFTModel(
     private val transactionFactory = blockchainConfiguration.getTransactionFactory()
 
     override fun postTransaction(tx: ByteArray) {
+
+        val tracer: Tracer = OpenTelemetryFactory.getOpenTelemetrySdk().getTracer(PostchainEBFTModel::class.simpleName.toString())
+        OpenTelemetryFactory.tracer = tracer
+        val span: Span = tracer.spanBuilder("postTransaction").startSpan()
+
+        span.makeCurrent()
+
         val sample = Timer.start(Metrics.globalRegistry)
 
+        val validateTransactionSpan = tracer.spanBuilder("validateTransaction").setParent(Context.current().with(span)).startSpan()
+        validateTransactionSpan.makeCurrent()
         transactionFactory.validateTransaction(tx)
-        val decodedTransaction = transactionFactory.decodeTransaction(tx)
+        validateTransactionSpan.end()
 
+        val decodeTransactionSpan = tracer.spanBuilder("decodeTransaction").setParent(Context.current().with(span)).startSpan()
+        decodeTransactionSpan.makeCurrent()
+        val decodedTransaction = transactionFactory.decodeTransaction(tx)
+        decodeTransactionSpan.end()
+
+        val checkCorrectnessSpan = tracer.spanBuilder("checkCorrectness").setParent(Context.current().with(span)).startSpan()
+        checkCorrectnessSpan.makeCurrent()
         decodedTransaction.checkCorrectness()
+        checkCorrectnessSpan.end()
 
         if (blockQueries.isTransactionConfirmed(decodedTransaction.getRID()).get()) {
             sample.stop(metrics.duplicateTransactions)
             throw DuplicateTnxException("Transaction already in database")
         }
 
+        val txEnqueueSpan = tracer.spanBuilder("txQueue.enqueue").setParent(Context.current().with(span)).startSpan()
+        txEnqueueSpan.makeCurrent()
         when (txQueue.enqueue(decodedTransaction)) {
             EnqueueTransactionResult.FULL -> {
                 sample.stop(metrics.fullTransactions)
+                txEnqueueSpan.addEvent("Transaction queue is full")
                 throw UnavailableException("Transaction queue is full")
             }
 
             EnqueueTransactionResult.INVALID -> {
                 sample.stop(metrics.invalidTransactions)
+                txEnqueueSpan.addEvent("Transaction is invalid")
                 throw InvalidTnxException("Transaction is invalid")
             }
 
             EnqueueTransactionResult.DUPLICATE -> {
                 sample.stop(metrics.duplicateTransactions)
+                txEnqueueSpan.addEvent("Transaction already in queue")
                 throw DuplicateTnxException("Transaction already in queue")
             }
 
             EnqueueTransactionResult.OK -> {
                 sample.stop(metrics.okTransactions)
+                txEnqueueSpan.addEvent("Transaction ok")
             }
         }
+        txEnqueueSpan.end()
+        span.end()
     }
 }
