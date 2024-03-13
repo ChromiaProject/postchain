1483dedd589eee75b07da33e864375c314d2ef34
Eugene Tykulov <eugene.tykulov@chromaway.com>
2024-03-13 16:15:44 +0200

    POS-1141: Add realdata test for confirm/{blockRid} endpoint

--- postchain-base/src/test/kotlin/net/postchain/api/rest/endpoint/RestApiConfirmBlockEndpointTest.kt
+++ postchain-base/src/test/kotlin/net/postchain/api/rest/endpoint/RestApiConfirmBlockEndpointTest.kt
@@ -7,17 +7,28 @@ import assertk.assertions.isEqualTo
 import io.restassured.RestAssured
 import io.restassured.http.ContentType
 import io.restassured.response.ValidatableResponse
+import net.postchain.PostchainContext
 import net.postchain.api.rest.BlockSignature
 import net.postchain.api.rest.controller.Model
+import net.postchain.api.rest.controller.PostchainModel
 import net.postchain.api.rest.controller.RestApi
+import net.postchain.base.BaseBlockHeader
 import net.postchain.base.BaseBlockWitness
+import net.postchain.base.BaseBlockWitnessBuilder
+import net.postchain.base.configuration.BaseBlockchainConfiguration
+import net.postchain.base.cryptoSystem
+import net.postchain.base.data.BaseBlockWitnessProvider
 import net.postchain.common.BlockchainRid
 import net.postchain.common.hexStringToByteArray
 import net.postchain.common.toHex
 import net.postchain.core.BlockRid
 import net.postchain.core.block.BlockDetail
+import net.postchain.core.block.BlockQueries
+import net.postchain.core.block.InitialBlockData
+import net.postchain.crypto.CryptoSystem
 import net.postchain.crypto.KeyPair
 import net.postchain.crypto.Secp256K1CryptoSystem
+import net.postchain.crypto.SigMaker
 import net.postchain.crypto.Signature
 import net.postchain.crypto.devtools.KeyPairHelper.privKey
 import net.postchain.crypto.devtools.KeyPairHelper.pubKey
@@ -25,21 +36,28 @@ import net.postchain.gtv.GtvDecoder
 import net.postchain.gtv.GtvEncoder
 import net.postchain.gtv.GtvNull
 import net.postchain.gtv.mapper.GtvObjectMapper
+import net.postchain.gtv.merkle.GtvMerkleHashCalculator
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
 import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.spy
 import org.mockito.kotlin.whenever
 import java.time.Clock
 import java.time.Instant
 import java.time.ZoneOffset
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionStage
 
 class RestApiConfirmBlockEndpointTest {
 
     private val basePath = "/api/v1"
     private lateinit var restApi: RestApi
-    private lateinit var model: Model
+    private lateinit var model0: Model
+    private lateinit var model1: Model
 
     // keys
     private val cs = Secp256K1CryptoSystem()
@@ -47,36 +65,89 @@ class RestApiConfirmBlockEndpointTest {
     private val sigMaker0 = cs.buildSigMaker(keyPair0)
     private val keyPair1 = KeyPair(pubKey(1), privKey(1))
     private val sigMaker1 = cs.buildSigMaker(keyPair1)
+    private val subjects = arrayOf(keyPair0.pubKey.data, keyPair1.pubKey.data)
 
     // block
     private val blockchainRID = BlockchainRid.buildFromHex("78967baa4768cbcef11c508326ffb13a956689fcb6dc3ba17f4b895cbb1577a1")
-    private val blockRid = BlockRid.buildFromHex("34ED10678AAE0414562340E8754A7CCD174B435B52C7F0A4E69470537AEE47E6")
+    private val prevBlockRID = BlockRid.buildFromHex("5AF85874B9CCAC197AA739585449668BE15650C534E08705F6D60A6993FE906D")
+
+    // block header
+    val blockHeader = BaseBlockHeader.make(
+            merkleHashCalculator = GtvMerkleHashCalculator(cryptoSystem),
+            iBlockData = InitialBlockData(
+                    blockchainRid = blockchainRID,
+                    blockIID = 0,
+                    chainID = 1L,
+                    prevBlockRID = prevBlockRID.data,
+                    height = 0,
+                    timestamp = 0L,
+                    blockHeightDependencyArr = emptyArray()
+            ),
+            rootHash = ByteArray(32) { 0 },
+            timestamp = 0L,
+            extraData = emptyMap()
+    )
 
     // signatures
-    private val signature0 = sigMaker0.signMessage(blockRid.data)
-    private val blockSignature0 = BlockSignature.fromSignature(signature0)
-    private val signature1 = sigMaker1.signMessage(blockRid.data)
-    private val blockSignature1 = BlockSignature.fromSignature(signature1)
+    private val signature0 = signBlock(blockHeader, cs, sigMaker0, subjects)
+    private val signature1 = signBlock(blockHeader, cs, sigMaker1, subjects)
     private val witness = BaseBlockWitness.fromSignatures(arrayOf(signature0, signature1))
 
     // BlockDetail
     private val block = BlockDetail(
-            rid = blockRid.data,
-            prevBlockRID = "5AF85874B9CCAC197AA739585449668BE15650C534E08705F6D60A6993FE906D".hexStringToByteArray(),
-            header = "023F9C7FBAFD92E53D7890A61B50B33EC0375FA424D60BD328AA2454408430C383".hexStringToByteArray(),
+            rid = blockHeader.blockRID,
+            prevBlockRID = blockHeader.prevBlockRID,
+            header = blockHeader.rawData,
             height = 0,
             transactions = listOf(),
             witness = witness.getRawData(),
             timestamp = 0
     )
 
+    private val unknownBlock = BlockRid.buildRepeat(0)
+
+    companion object {
+        fun signBlock(blockHeader: BaseBlockHeader, cryptoSystem: CryptoSystem, sigMaker: SigMaker, subjects: Array<ByteArray>): Signature {
+            val witnessBuilder = BaseBlockWitnessProvider(cryptoSystem, sigMaker, subjects)
+                    .createWitnessBuilderWithOwnSignature(blockHeader) as BaseBlockWitnessBuilder
+            return witnessBuilder.getMySignature()
+        }
+    }
+
     @BeforeEach
     fun setup() {
-        model = mock {
-            on { chainIID } doReturn 1L
-            on { blockchainRid } doReturn blockchainRID
-            on { live } doReturn true
+        val bcConfig0: BaseBlockchainConfiguration = mock {
+            on { chainID } doReturn 1L
+            on { blockSigMaker } doReturn sigMaker0
+            on { signers } doReturn listOf(keyPair0.pubKey.data, keyPair1.pubKey.data)
+            on { rawConfig } doReturn GtvNull
         }
+
+        val bcConfig1: BaseBlockchainConfiguration = mock {
+            on { chainID } doReturn 1L
+            on { blockSigMaker } doReturn sigMaker1
+            on { signers } doReturn listOf(keyPair0.pubKey.data, keyPair1.pubKey.data)
+            on { rawConfig } doReturn GtvNull
+        }
+
+        val blockQueries: BlockQueries = mock {
+            val blockResult: CompletionStage<BlockDetail?> = CompletableFuture.completedStage(block)
+            on { getBlock(eq(block.rid), any()) } doReturn blockResult
+            val unknownBlockResult: CompletionStage<BlockDetail?> = CompletableFuture.completedStage(null)
+            on { getBlock(eq(unknownBlock.data), any()) } doReturn unknownBlockResult
+        }
+
+        val postchainContext: PostchainContext = mock {
+            on { cryptoSystem } doReturn cs
+        }
+
+        model0 = spy(PostchainModel(
+                bcConfig0, mock(), blockQueries, blockchainRID, mock(), postchainContext, mock(), 0L
+        ))
+
+        model1 = spy(PostchainModel(
+                bcConfig1, mock(), blockQueries, blockchainRID, mock(), postchainContext, mock(), 0L
+        ))
 
         restApi = RestApi(0, basePath, gracefulShutdown = false, clock = Clock.fixed(Instant.EPOCH, ZoneOffset.UTC))
     }
@@ -88,9 +159,7 @@ class RestApiConfirmBlockEndpointTest {
 
     @Test
     fun `Confirm block returns signature in GTV`() {
-        val rawSignature = runQuery(
-                blockSignature0, ContentType.BINARY, 200, ContentType.BINARY
-        ).extract().asByteArray()
+        val rawSignature = runQuery(model0, block.rid, ContentType.BINARY, 200, ContentType.BINARY).extract().asByteArray()
         assertThat(rawSignature).isEqualTo(
                 GtvEncoder.encodeGtv(GtvObjectMapper.toGtvDictionary(BlockSignature.fromSignature(signature0)))
         )
@@ -99,14 +168,12 @@ class RestApiConfirmBlockEndpointTest {
         // asserting that subjectID is what we sent a confirmation request to
         assertThat(blockSignature.subjectID).isEqualTo(signature0.subjectID)
         // verifying signature
-        assertThat(cs.makeVerifier()(block.rid, blockSignature.toSignature())).isEqualTo(true)
+        assertThat(cs.verifyDigest(block.rid, blockSignature.toSignature())).isEqualTo(true)
     }
 
     @Test
     fun `Confirm block returns signature in JSON`() {
-        val blockSignatureJsonPath = runQuery(
-                blockSignature1, ContentType.JSON, 200, ContentType.JSON
-        ).extract().jsonPath()
+        val blockSignatureJsonPath = runQuery(model1, block.rid, ContentType.JSON, 200, ContentType.JSON).extract().jsonPath()
 
         // asserting that json has only two keys
         assertThat(
@@ -122,12 +189,12 @@ class RestApiConfirmBlockEndpointTest {
         val signature = Signature(
                 signature1.subjectID,
                 blockSignatureJsonPath.getString("data").hexStringToByteArray())
-        assertThat(cs.makeVerifier()(block.rid, signature)).isEqualTo(true)
+        assertThat(cs.verifyDigest(block.rid, signature)).isEqualTo(true)
     }
 
     @Test
     fun `Confirm block returns signature in JSON by default`() {
-        val res = runQuery(blockSignature0, null, 200, ContentType.JSON)
+        val res = runQuery(model0, block.rid, null, 200, ContentType.JSON)
         assertThat(res.extract().jsonPath().getString("subjectID")).isEqualTo(
                 signature0.subjectID.toHex()
         )
@@ -135,7 +202,7 @@ class RestApiConfirmBlockEndpointTest {
 
     @Test
     fun `Confirm block returns GtvNull for unknown block`() {
-        val res = runQuery(null, ContentType.BINARY, 200, ContentType.BINARY)
+        val res = runQuery(model0, unknownBlock.data, ContentType.BINARY, 200, ContentType.BINARY)
         assertThat(res.extract().asByteArray()).isEqualTo(
                 GtvEncoder.encodeGtv(GtvNull)
         )
@@ -143,34 +210,46 @@ class RestApiConfirmBlockEndpointTest {
 
     @Test
     fun `Confirm block returns JSON null for unknown block`() {
-        val res = runQuery(null, ContentType.JSON, 200, ContentType.JSON)
+        val res = runQuery(model0, unknownBlock.data, ContentType.JSON, 200, ContentType.JSON)
         assertThat(res.extract().response().asString()).isEqualTo("null")
     }
 
     @Test
     fun `Confirm block returns signature in JSON by default for unknown block`() {
-        val response = runQuery(null, null, 200, ContentType.JSON)
+        val response = runQuery(model0, unknownBlock.data, null, 200, ContentType.JSON)
         assertThat(response.extract().response().asString()).isEqualTo("null")
     }
 
     @Test
     fun `Confirm block throws an error for non-live blockchain`() {
-        whenever(model.live).thenReturn(false)
-        runQuery(blockSignature0, ContentType.BINARY, 503, ContentType.BINARY)
-        runQuery(blockSignature0, ContentType.JSON, 503, ContentType.JSON)
-        runQuery(blockSignature0, null, 503, ContentType.JSON)
+        whenever(model0.live).thenReturn(false)
+        runQuery(model0, block.rid, ContentType.BINARY, 503, ContentType.BINARY)
+        runQuery(model0, block.rid, ContentType.JSON, 503, ContentType.JSON)
+        runQuery(model0, block.rid, null, 503, ContentType.JSON)
     }
 
-    private fun runQuery(confirmBlockResult: BlockSignature?, accept: ContentType?, statusCode: Int, contentType: ContentType): ValidatableResponse {
-        whenever(model.confirmBlock(BlockRid(block.rid))).thenReturn(confirmBlockResult)
+    private fun runMockedQuery(confirmBlockResult: BlockSignature?, accept: ContentType?, statusCode: Int, contentType: ContentType): ValidatableResponse {
+        whenever(model0.confirmBlock(BlockRid(block.rid))).thenReturn(confirmBlockResult)
 
-        restApi.attachModel(blockchainRID, model)
+        restApi.attachModel(blockchainRID, model0)
 
         return RestAssured.given().basePath(basePath).port(restApi.actualPort())
                 .apply {
                     if (accept != null) header("Accept", accept)
                 }
                 .get("/blocks/$blockchainRID/confirm/${block.rid.toHex()}")
+                .then()
+                .statusCode(statusCode)
+                .contentType(contentType)
+    }
+
+    private fun runQuery(model: Model, blockRid: ByteArray, accept: ContentType?, statusCode: Int, contentType: ContentType): ValidatableResponse {
+        restApi.attachModel(blockchainRID, model)
+        return RestAssured.given().basePath(basePath).port(restApi.actualPort())
+                .apply {
+                    if (accept != null) header("Accept", accept)
+                }
+                .get("/blocks/$blockchainRID/confirm/${blockRid.toHex()}")
                 .then()
                 .statusCode(statusCode)
                 .contentType(contentType)
