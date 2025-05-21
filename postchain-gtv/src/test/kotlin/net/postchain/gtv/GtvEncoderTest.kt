83baa17a47d60e316c282bd95d87cb6e6ef0c174
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-05-21 20:31:38 +0200

    Some more GTV encoding tests

--- postchain-gtv/src/test/kotlin/net/postchain/gtv/GtvEncoderTest.kt
+++ postchain-gtv/src/test/kotlin/net/postchain/gtv/GtvEncoderTest.kt
@@ -13,7 +13,7 @@ import kotlin.system.measureTimeMillis
 class GtvEncoderTest {
 
     @Test
-    fun testGtvNull() {
+    fun `GTV null`() {
         val expected = GtvNull
         val b = GtvEncoder.encodeGtv(expected)
         val result = GtvDecoder.decodeGtv(b)
@@ -21,7 +21,7 @@ class GtvEncoderTest {
     }
 
     @Test
-    fun testGtvInteger() {
+    fun `positive integer`() {
         val expected = GtvInteger(Long.MAX_VALUE)
         val b = GtvEncoder.encodeGtv(expected)
         val result = GtvDecoder.decodeGtv(b)
@@ -30,7 +30,25 @@ class GtvEncoderTest {
     }
 
     @Test
-    fun testGtvBigInteger() {
+    fun `zero integer`() {
+        val expected = GtvInteger(0)
+        val b = GtvEncoder.encodeGtv(expected)
+        val result = GtvDecoder.decodeGtv(b)
+        assertEquals(expected, result)
+        assertEquals(expected.asInteger().toString(10), result.asInteger().toString(10))
+    }
+
+    @Test
+    fun `negative integer`() {
+        val expected = GtvInteger(Long.MIN_VALUE)
+        val b = GtvEncoder.encodeGtv(expected)
+        val result = GtvDecoder.decodeGtv(b)
+        assertEquals(expected, result)
+        assertEquals(expected.asInteger().toString(10), result.asInteger().toString(10))
+    }
+
+    @Test
+    fun `positive big integer`() {
         val expected = GtvBigInteger(BigInteger.valueOf(Long.MAX_VALUE).pow(3))
         val b = GtvEncoder.encodeGtv(expected)
         val result = GtvDecoder.decodeGtv(b)
@@ -39,7 +57,25 @@ class GtvEncoderTest {
     }
 
     @Test
-    fun testGtvString() {
+    fun `zero big integer`() {
+        val expected = GtvBigInteger(BigInteger.valueOf(0))
+        val b = GtvEncoder.encodeGtv(expected)
+        val result = GtvDecoder.decodeGtv(b)
+        assertEquals(expected, result)
+        assertEquals(expected.asBigInteger().toString(10), result.asBigInteger().toString(10))
+    }
+
+    @Test
+    fun `negative big integer`() {
+        val expected = GtvBigInteger(BigInteger("-92233720368547758078"))
+        val b = GtvEncoder.encodeGtv(expected)
+        val result = GtvDecoder.decodeGtv(b)
+        assertEquals(expected, result)
+        assertEquals(expected.asBigInteger().toString(10), result.asBigInteger().toString(10))
+    }
+
+    @Test
+    fun `ASCII string`() {
         val expected = GtvString("postchain")
         val b = GtvEncoder.encodeGtv(expected)
         val result = GtvDecoder.decodeGtv(b)
@@ -47,7 +83,7 @@ class GtvEncoderTest {
     }
 
     @Test
-    fun testGtvStringNonAscii() {
+    fun `UTF-8 string`() {
         val expected = GtvString("Räksmörgås!")
         val b = GtvEncoder.encodeGtv(expected)
         val result = GtvDecoder.decodeGtv(b)
@@ -55,8 +91,16 @@ class GtvEncoderTest {
     }
 
     @Test
-    fun testGtvByteArray() {
-        val bytes =  ByteArray(3)
+    fun `empty string`() {
+        val expected = GtvString("")
+        val b = GtvEncoder.encodeGtv(expected)
+        val result = GtvDecoder.decodeGtv(b)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `byte array`() {
+        val bytes = ByteArray(3)
         bytes[0] = 0x10
         bytes[1] = 0x1A
         bytes[2] = 0x68
@@ -67,8 +111,17 @@ class GtvEncoderTest {
     }
 
     @Test
-    fun testGtvArray() {
-        val gtvArray = Array<Gtv>(3) { GtvString("postchain")}
+    fun `empty byte array`() {
+        val bytes = ByteArray(0)
+        val expected = GtvByteArray(bytes)
+        val b = GtvEncoder.encodeGtv(expected)
+        val result = GtvDecoder.decodeGtv(b)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun array() {
+        val gtvArray = Array<Gtv>(3) { GtvString("postchain") }
         val expected = GtvArray(gtvArray)
         val b = GtvEncoder.encodeGtv(expected)
         val result = GtvDecoder.decodeGtv(b)
@@ -76,7 +129,15 @@ class GtvEncoderTest {
     }
 
     @Test
-    fun testGtvDictionary() {
+    fun `empty array`() {
+        val expected = GtvArray(arrayOf())
+        val b = GtvEncoder.encodeGtv(expected)
+        val result = GtvDecoder.decodeGtv(b)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun dictionary() {
         val map = mapOf("name" to GtvString("postchain"), "age" to GtvInteger(42))
         val expected = GtvDictionary.build(map)
         val b = GtvEncoder.encodeGtv(expected)
@@ -85,11 +146,19 @@ class GtvEncoderTest {
     }
 
     @Test
-    fun stressTestGtv() {
-        val size = (1024 * 1024 * 4) / 10  // that could make gtv size is around 2.7 MiB
+    fun `empty dictionary`() {
+        val expected = GtvDictionary.build(mapOf())
+        val b = GtvEncoder.encodeGtv(expected)
+        val result = GtvDecoder.decodeGtv(b)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `stress test`() {
+        val size = (1024 * 1024 * 4) / 10  // that could make gtv size around 2.7 MiB
         // TODO: this test is 10 times smaller than it should be because we trigger OOM
         // currently it requires >2 GiB to compute hash
-        val gtvArray  = (1..size).map { GtvInteger( it.toLong() ) }.toTypedArray()
+        val gtvArray = (1..size).map { GtvInteger(it.toLong()) }.toTypedArray()
         var encoded: ByteArray
         val gtv = GtvArray(gtvArray)
         val serializationTime = measureTimeMillis {
